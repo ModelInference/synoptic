a14e97a43676123d433a132c5e29963857493ed1
sigurdschneider <sigurdschneider@googlemail.com>
2010-10-10 01:05:01 +0000

    Fixing.

--- src/benchmarks/ReverseTracerouteBenchmark.java
+++ src/benchmarks/ReverseTracerouteBenchmark.java
@@ -15,7 +15,7 @@ import benchmarks.Benchmark.ConfigureableBenchmark;
  *
  */
 public class ReverseTracerouteBenchmark {
-	static private final int REPETITIONS = 2;
+	static private final int REPETITIONS = 1;
 	static private  ConfigureableBenchmark<Integer> benchmarkFactory = new ConfigureableBenchmark<Integer>() {
 
 		@Override
@@ -30,7 +30,7 @@ public class ReverseTracerouteBenchmark {
 	static public void main(String[] args) {
 		Benchmark<Integer> benchmark = new Benchmark<Integer>(REPETITIONS, benchmarkFactory);
 		ArrayList<Integer> arguments = new ArrayList<Integer>();
-		for (int i = 1; i < 10; i++) {
+		for (int i = 1; i < 30; i++) {
 			arguments.add(i);
 		}
 		benchmark.run(arguments);
