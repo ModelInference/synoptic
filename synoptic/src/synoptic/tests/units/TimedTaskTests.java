ab48eecf5f16ea63d50704d86eaecd68d3a4a9b5
bestchai <bestchai@localhost>
2011-01-15 00:02:42 +0000

    added someunit test skeletons, slight change to final output filenames

---
+++ synoptic/src/synoptic/tests/units/TimedTaskTests.java
@@ -0,0 +1,26 @@
+package synoptic.tests.units;
+
+import org.junit.Test;
+
+/**
+ * Tests for synoptic.benchmarks.TimedTask class
+ * 
+ * @author ivan
+ */
+public class TimedTaskTests {
+	/**
+	 * Make sure we can run a task.
+	 */
+	@Test
+	public void runTaskTest() {
+		// TODO
+	}
+	
+	/**
+	 * Run and time a task.
+	 */
+	@Test
+	public void runAndTimeTaskTest() {
+		// TODO
+	}
+}
