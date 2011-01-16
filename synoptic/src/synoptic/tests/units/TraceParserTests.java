e85424cabcb428cb6c417923df69a1f8b143d113
bestchai <bestchai@localhost>
2011-01-16 09:34:58 +0000

    Added a -i parser option to ignore non-matching lines, refactored parser code to make it easier to test.

--- synoptic/src/synoptic/tests/units/TraceParserTests.java
+++ synoptic/src/synoptic/tests/units/TraceParserTests.java
@@ -1,15 +1,28 @@
 package synoptic.tests.units;
 
+import java.util.List;
+
 import org.junit.Test;
+import static org.junit.Assert.*;
+  
+import synoptic.main.ParseException;
+import synoptic.main.TraceParser;
+import synoptic.main.TraceParser.Occurrence;
 
 
 public class TraceParserTests {
 	/**
 	 * Parse a log with implicit time that increments on each log line.
+	 * @throws ParseException 
 	 */
 	@Test
-	public void parseImplicitTimeTest() {
-		// TODO
+	public void parseImplicitTimeTest() throws ParseException {
+		TraceParser parser = new TraceParser();
+		String trace = "a\nb\nc\n";
+		parser.addRegex("^(?<TYPE>)$");
+		// Purposefully don't handle the ParseException because the parse is broken
+		List<Occurrence> occs = parser.parseTraceString(trace, new String("test"), -1);
+		// TODO: test occs here.
 	}
 	
 	/**
