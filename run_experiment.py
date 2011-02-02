21bb87044487279ab5c1d868dc677bda26b4e1b7
mgsloan <mgsloan@localhost>
2011-02-02 08:43:29 +0000

    Added a menagerie which only now contains a few examples.

--- run_experiment.py
+++ run_experiment.py
@@ -26,19 +26,29 @@ peterson_three = peterson_one + " "    \
                + ppath + "n5-1-s3.txt" \
                + ppath + "n5-1-s4.txt"
 
+if "menagerie" in sys.argv:
+    prefix = vjava + (" -f " if "-f" in sys.argv else "") + \
+        " -r '(?<TYPE>.*)' -s '^$' -o output/menagerie-"
+    path = " traces/abstract/menagerie/"
+    calls = [prefix + os.path.splitext(x)[0] + path + x
+             for x in os.listdir(path.strip())]
+    print calls
+    map(os.system, calls)
+    quit()
+
 for ar in sys.argv[1:]:
     if ar[0] == '-':
         continue
     values = { "peterson_all": peterson_args + peterson_all,
-               "peterson_debug": " --debugParse=true" + peterson_args +
-               peterson_one,
-               "peterson_some": peterson_args + peterson_three}
+               "peterson_debug": " --debugParse=true" + peterson_args + peterson_one,
+               "peterson_some": peterson_args + peterson_three,
+             }
     if ar in values:
         cmd = vprofjava if "-prof" in sys.argv else vjava
         if "-f" in sys.argv:
-            cmd += " -f"
+            cmd += " -f "
         cmd += values[ar]
         print "Executing: " + cmd
         os.system(cmd);
     else:
-        print ar + " not found. Available: " + str(values.keys())
+        print ar + " not found. Available: menagerie, " + str(values.keys())
