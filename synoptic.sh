58e85bdbae7c1009fec08e18a3d164d48db0ff6b
bestchai <bestchai@localhost>
2011-02-16 21:25:26 +0000

    Added junit lib, modified classpath and synoptic.sh to use it.

--- synoptic.sh
+++ synoptic.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -ea -cp ./lib/plume.jar:./synoptic/bin/:./lib/jung/* synoptic.main.Main $*
+java -ea -cp ./lib/junit-4.9b2.jar:./lib/plume.jar:./synoptic/bin/:./lib/jung/* synoptic.main.Main $*
