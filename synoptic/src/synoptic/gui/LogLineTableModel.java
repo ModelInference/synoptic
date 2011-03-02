88c88236495b2b8bdc5a9e6898aa681d2988b935
jenny.abrahamson <jenny.abrahamson@localhost>
2011-03-02 00:07:31 +0000

    *added panel at the bottom of the gui that displays log lines when users double click on a node *removed the mode combo box, mapped transforming to right mouse clicks and picking to left mouse clicks (via ModalMousePlugin class)

---
+++ synoptic/src/synoptic/gui/LogLineTableModel.java
@@ -0,0 +1,38 @@
+package synoptic.gui;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * Extension of AbstractTabelModel which allows the log line table to be updated with new data
+ * 
+ */
+@SuppressWarnings("serial")
+public class LogLineTableModel extends AbstractTableModel {
+	private final String [] columnNames = new String [] {"Line", "File"};
+	private Object[][] data;
+
+	public LogLineTableModel(Object[][]data) {
+		this.data = data;
+	}
+
+	@Override
+	public int getRowCount() {
+		return data.length;
+	}
+
+	@Override
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		return data[rowIndex][columnIndex];
+	}
+	
+	public void setData(Object[][] newData) {  
+		data = newData;  
+		fireTableDataChanged();  
+	}  
+
+}
---
+++ synoptic/src/synoptic/gui/ModalMousePlugin.java
@@ -0,0 +1,41 @@
+package synoptic.gui;
+
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
+
+import synoptic.model.Partition;
+import synoptic.model.interfaces.INode;
+import synoptic.model.interfaces.ITransition;
+
+
+import edu.uci.ics.jung.visualization.RenderContext;
+import edu.uci.ics.jung.visualization.control.AnimatedPickingGraphMousePlugin;
+import edu.uci.ics.jung.visualization.control.CrossoverScalingControl;
+import edu.uci.ics.jung.visualization.control.EditingModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.PickingGraphMousePlugin;
+import edu.uci.ics.jung.visualization.control.RotatingGraphMousePlugin;
+import edu.uci.ics.jung.visualization.control.ScalingGraphMousePlugin;
+import edu.uci.ics.jung.visualization.control.ShearingGraphMousePlugin;
+import edu.uci.ics.jung.visualization.control.TranslatingGraphMousePlugin;
+
+/**
+ * Extension of EditingModalGraphMouse which maps transforming mode to right mouse clicks, picking
+ * mode to left mouse clicks, and removes editing mode functionality
+ * 
+ */
+public class ModalMousePlugin extends EditingModalGraphMouse<INode<Partition>, ITransition<Partition>> {
+
+	public ModalMousePlugin(RenderContext <INode<Partition>, ITransition<Partition>> rc) {
+		super(rc, null, null);
+	}
+
+   protected void loadPlugins() {
+	   add(new PickingGraphMousePlugin<INode<Partition>, ITransition<Partition>>());
+       add(new AnimatedPickingGraphMousePlugin<INode<Partition>, ITransition<Partition>>());
+       add(new TranslatingGraphMousePlugin(InputEvent.BUTTON3_MASK));
+       add(new ScalingGraphMousePlugin(new CrossoverScalingControl(), 0));
+       add(new RotatingGraphMousePlugin(MouseEvent.BUTTON3_MASK | MouseEvent.SHIFT_MASK));
+       add(new ShearingGraphMousePlugin(MouseEvent.BUTTON3_MASK | MouseEvent.CTRL_MASK));
+   }
+
+}
