55012b6df720b6cb8551bff06cfd4dcc88693729
bestchai <bestchai@localhost>
2011-04-22 18:37:42 +0000

    Added shopping cart example, and some older trace processing files, commented .sh files

--- synoptic_dump_heapmap.sh
+++ synoptic_dump_heapmap.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+# Runs Synoptic from the compiled class files, passing all command
+# line argument directly to main().
+
+# Used for debugging OutOfMemoryErrors by dumping the heap information
+# in hprof file format to OutOfMemoryErrorHeapDump.hprof
+
 java -ea -XX:HeapDumpPath=./OutOfMemoryErrorHeapDump.hprof -XX:+HeapDumpOnOutOfMemoryError -cp ./lib/junit-4.9b2.jar:./lib/plume.jar:./synoptic/bin/:./lib/jung/* synoptic.main.Main $*
