5bb7e6b3444329d3d03caf96ea516a3e279acf7f
Ivan Beschastnikh <ivan@cs.washington.edu>
2011-07-11 13:26:27 -0700

    Merged e37eea88f33e and 1b01ddaf92fb

--- synopticgwt/src/synopticgwt/server/SynopticService.java
+++ synopticgwt/src/synopticgwt/server/SynopticService.java
@@ -388,17 +388,16 @@ public class SynopticService extends RemoteServiceServlet implements
      */
     @Override
     public String exportModel() throws Exception {
-        retrieveSessionState();
-        GraphVizExporter exporter = new GraphVizExporter();
-        Calendar now = Calendar.getInstance();
-        String filename = "userexport/" + now.getTimeInMillis()
-                + "exportmodel.dot";
-        exporter.exportAsDotAndPngFast(filename, pGraph);
-        File file = new File(filename + ".png");
-        while (!file.exists()) {
-            // file = new File(filename + ".png");
-        }
-        return filename;
+    	retrieveSessionState();
+    	GraphVizExporter exporter = new GraphVizExporter();
+    	Calendar now = Calendar.getInstance();
+    	String filename = "userexport/"
+    		+ now.getTimeInMillis() + "exportmodel.dot";
+    	exporter.exportAsDotAndPngFast(filename, pGraph);
+    	File file = new File(filename + ".png");
+    	while (!file.exists()) {
+    	}
+    	return filename;
     }
 
 }
