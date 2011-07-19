c44d868f8d3f09f017f6bc285817772ad128ee7b
Ivan Beschastnikh <ivan@cs.washington.edu>
2011-07-19 16:42:47 -0700

    Updated doc_usage.sh script to work with hg.

--- doc_usage.sh
+++ doc_usage.sh
@@ -1,38 +1,62 @@
 #!/bin/sh
 #
 # Documents Synoptic's usage screen (generated with the -H option) to
-# a wiki page -- 'DocsUsageScreen'
+# a wiki page -- 'DocsUsageScreen':
+# http://code.google.com/p/synoptic/wiki/DocsUsageScreen
 #
 # This script must be run manually, and will only work if executed
-# from within trunk, i.e. /svn/trunk/
+# from within the default synoptic code branch. This script assume
+# that the working copy is at the tip.
 #
 # Preferably this script will be run whenever synoptic command
 # line options change.
 #
 
+if [ -z "$1" ]
+then
+ echo "Usage: ./doc_usage.sh [wiki-path]"
+ echo "       wiki-path : path of local working copy of the synoptic wiki Hg repository"
+ exit
+fi 
+
+# Assumes that the current dir is the synoptic repo.
+synoptic_repo=`pwd`
+
+# First argument is the location of the Synoptic wiki repo.
+synoptic_wiki_repo=$1
+
 # Location of the usage screen wiki page.
-usagef=../wiki/DocsUsageScreen.wiki
+usagef=${synoptic_wiki_repo}/DocsUsageScreen.wiki
 
-# Wipe out the old wiki page content.
-rm $usagef
+# 1. Pull and update the wiki repository
+cd ${synoptic_wiki_repo} && hg pull && hg up
 
-# Generate the new wiki page.
+# 2. Wipe out the old usage wiki page.
+cd ${synoptic_wiki_repo} && rm $usagef
+
+# 3. Generate the new wiki page
 echo "#summary Lists the command line usage screen" >> $usagef
 echo "" >> $usagef
 echo "= Command Line Options =" >> $usagef
 echo "{{{" >> $usagef
-./synoptic.sh -H >> $usagef
+cd ${synoptic_repo} && ./synoptic.sh -H >> $usagef
 echo "}}}" >> $usagef
 echo >> $usagef
 
-echo "Last Updated r\c" >> $usagef
-rev=`cd ../wiki/ && svn up | grep 'At revision' | awk '{print $3}' | tr -d '.'`
-echo $(( rev+1 )) >> $usagef
+# 4. Determine the current revision for the synoptic repository
+# (assumed to be the current dir)
+echo "As of revision \c" >> $usagef
+cd ${synoptic_repo} && hg tip --template "{node|short}" >> $usagef
+echo >> $usagef
+echo >> $usagef
 
-syn_vers=`./synoptic.sh -V`
-echo ${syn_vers} >> $usagef
+# 5. Determine the synoptic version by running synoptic.sh (assuming
+# the current dir is the synoptic repo).
+cd ${synoptic_repo} && ./synoptic.sh -V >> $usagef
 echo >> $usagef
 echo "*Note: this page is auto-generated. Do not edit.*" >> $usagef
 
-# Commit the new wiki page.
-cd ../wiki/ && svn commit -m 'Updated usage screen docs'
+# 6. Commit and push the new wiki page.
+cd ${synoptic_wiki_repo} && hg commit -m 'Updated usage screen docs' $usagef && hg push
+
+
