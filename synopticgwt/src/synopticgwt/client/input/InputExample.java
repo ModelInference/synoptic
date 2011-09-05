ff1367fb5bfa9f82cd01e0a928b5523a36146cd3
Ivan Beschastnikh <ivan@cs.washington.edu>
2011-09-05 13:42:43 +0200

    Added more log examples.

--- synopticgwt/src/synopticgwt/client/input/InputExample.java
+++ synopticgwt/src/synopticgwt/client/input/InputExample.java
@@ -5,12 +5,14 @@ public class InputExample {
     public String logText;
     public String regExpText;
     public String partitionRegExpText;
+    public String separatorRegExpText;
 
     public InputExample(String name, String logText, String regExpText,
-            String partitionRegExpText) {
+            String partitionRegExpText, String separatorRegExpText) {
         this.name = name;
         this.logText = logText;
         this.regExpText = regExpText;
         this.partitionRegExpText = partitionRegExpText;
+        this.separatorRegExpText = separatorRegExpText;
     }
 }
