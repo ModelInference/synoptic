cb4873fac1779d07806a5f0c9dfe97bd1da50170
kevin.a.thai <kevin.a.thai@localhost>
2011-10-11 01:02:21 -0700

    Removed unused imports.

--- synopticgwt/src/synopticgwt/client/input/InputSubTab.java
+++ synopticgwt/src/synopticgwt/client/input/InputSubTab.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
