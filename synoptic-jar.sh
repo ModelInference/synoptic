29438f955eb90147d9d7019c1e387cb1022d41bc
Ivan Beschastnikh <ivan@cs.washington.edu>
2011-11-19 15:50:12 -0800

    Fixed classpath in synoptic's build.xml, and simplified synoptic sh scripts.

--- synoptic-jar.sh
+++ synoptic-jar.sh
@@ -3,4 +3,4 @@
 # Runs Synoptic from the compiled JAR file, passing all command
 # line argument directly to main().
 
-java -ea -cp ./lib/junit-4.9b2.jar:./lib/plume.jar:./synoptic/bin/:./lib/jung/* -jar ./lib/synoptic.jar $*
+java -ea -jar ./lib/synoptic.jar $*
--- synoptic.sh
+++ synoptic.sh
@@ -3,4 +3,4 @@
 # Runs Synoptic from the compiled class files, passing all command
 # line argument directly to main().
 
-java -ea -cp ./lib/junit-4.9b2.jar:./lib/plume.jar:./synoptic/bin/:./lib/jung/* synoptic.main.Main $*
+java -ea -cp ./lib/junit-4.9b2.jar:./lib/plume.jar:./synoptic/bin/ synoptic.main.Main $*
