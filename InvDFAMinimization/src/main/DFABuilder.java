99e9c8f99dec35a3ae756bcb1bdd7538f1e9c178
Ivan Beschastnikh <ivan@cs.washington.edu>
2011-11-19 16:14:55 -0800

    Updated build.xml, and re-formatted and fixed minor warning in DFABuilder

--- InvDFAMinimization/src/main/DFABuilder.java
+++ InvDFAMinimization/src/main/DFABuilder.java
@@ -29,164 +29,182 @@ import synoptic.model.export.GraphExporter;
  */
 public class DFABuilder {
 
-	// DFA of the initial model.
-	private Automaton initialModel;
+    // DFA of the initial model.
+    private Automaton initialModel;
 
-	// DFA of the final model.
-	private Automaton finalModel;
+    // DFA of the final model.
+    private Automaton finalModel;
 
-	// Map of encoding characters to EventType labels.
-	private Map<Character, String> encodings;
-	
-	// Number of possible unicode characters, for encoding EventType labels.
-	private static final int UNICODE_CHARS = 0x10FFFF;
+    // Map of encoding characters to EventType labels.
+    private Map<Character, String> encodings;
 
-	// Character encodings for the initial and terminal EventTypes.
-	private static char INITIAL;
-	private static char TERMINAL;
+    // Number of possible unicode characters, for encoding EventType labels.
+    private static final int UNICODE_CHARS = 0x10FFFF;
 
-	public DFABuilder(TemporalInvariantSet NIFbys, TemporalInvariantSet minedInvariants) {
-		this.encodings = new HashMap<Character, String>();
-		initialModel = buildInitialDFAModel(NIFbys);
-		finalModel = buildFinalModel(minedInvariants);
-	}
+    // Character encodings for the initial and terminal EventTypes.
+    private static char INITIAL;
+    private static char TERMINAL;
 
-	/* Returns a character encoding for the given EventType. Updates the encodings map and possibly
-	 * INITIAL/TERMINAL characters if this EventType has not been seen before.
-	 * TODO: Address possibility of encoding collisions */
-	private char getEncoding(EventType event) {
-		char encoding = (char) (event.hashCode() % UNICODE_CHARS);
-		if (!encodings.containsKey(encoding)) {
-			if (event.isInitialEventType()) {
-				INITIAL = encoding;
-			} else if (event.isTerminalEventType()) {
-				TERMINAL = encoding;
-			}
-			encodings.put(encoding, event.toString());
-		}
-		return encoding;
-	}
+    public DFABuilder(TemporalInvariantSet NIFbys,
+            TemporalInvariantSet minedInvariants) {
+        this.encodings = new HashMap<Character, String>();
+        initialModel = buildInitialDFAModel(NIFbys);
+        finalModel = buildFinalModel(minedInvariants);
+    }
 
-	/* Builds the initial DFAModel from the given set of NIFby invariants. */
-	private Automaton buildInitialDFAModel(TemporalInvariantSet NIFbys) {
+    /*
+     * Returns a character encoding for the given EventType. Updates the
+     * encodings map and possibly INITIAL/TERMINAL characters if this EventType
+     * has not been seen before. TODO: Address possibility of encoding
+     * collisions
+     */
+    private char getEncoding(EventType event) {
+        char encoding = (char) (event.hashCode() % UNICODE_CHARS);
+        if (!encodings.containsKey(encoding)) {
+            if (event.isInitialEventType()) {
+                INITIAL = encoding;
+            } else if (event.isTerminalEventType()) {
+                TERMINAL = encoding;
+            }
+            encodings.put(encoding, event.toString());
+        }
+        return encoding;
+    }
 
-		Automaton model = intersectModelWith(BasicAutomata.makeAnyString(), NIFbys);
+    /* Builds the initial DFAModel from the given set of NIFby invariants. */
+    private Automaton buildInitialDFAModel(TemporalInvariantSet NIFbys) {
 
-		// Add INITIAL/TERMINAL constraints.
-		model = BasicOperations.intersection(model, new RegExp(
-				initialAndTerminalRegex()).toAutomaton());
+        Automaton model = intersectModelWith(BasicAutomata.makeAnyString(),
+                NIFbys);
 
-		// Use the encodings to limit the alphabet of the automaton.
-		model = BasicOperations.intersection(model,
-				new RegExp(alphabetRegex()).toAutomaton());
+        // Add INITIAL/TERMINAL constraints.
+        model = BasicOperations.intersection(model, new RegExp(
+                initialAndTerminalRegex()).toAutomaton());
 
-		// Optimize by minimizing the model.
-		model.minimize();
+        // Use the encodings to limit the alphabet of the automaton.
+        model = BasicOperations.intersection(model,
+                new RegExp(alphabetRegex()).toAutomaton());
 
-		return model;
-	}
+        // Optimize by minimizing the model.
+        model.minimize();
 
-	/* Builds the final DFAModel from the initial model and the given set of mined invariants. */
-	private Automaton buildFinalModel(TemporalInvariantSet minedInvariants) {
-		Automaton finalModel = intersectModelWith(initialModel.clone(), minedInvariants);
-		finalModel.minimize();
-		return finalModel;
-	}
-	
-	/* Intersects the given model with DFAs for all of the given invariants */
-	private Automaton intersectModelWith(Automaton model, TemporalInvariantSet invariants) {
-		for (ITemporalInvariant invariant : invariants) {
-			char source = getEncoding(invariant.getFirst());
-			char dest = getEncoding(invariant.getSecond());
-			
-			RegExp re = new RegExp(invariant.getRegex(source, dest));
-			model = BasicOperations.intersection(model, re.toAutomaton());
-		}
-		return model;
-	}
+        return model;
+    }
 
-	/* Returns a regular expression describing the INITIAL and TERMINAL constraints for the DFA. */
-	private String initialAndTerminalRegex() {
-		return INITIAL + "[^" + INITIAL + TERMINAL + "]*" + TERMINAL;
-	}
+    /*
+     * Builds the final DFAModel from the initial model and the given set of
+     * mined invariants.
+     */
+    private Automaton buildFinalModel(TemporalInvariantSet minedInvariants) {
+        Automaton finalModel_ = intersectModelWith(initialModel.clone(),
+                minedInvariants);
+        finalModel_.minimize();
+        return finalModel_;
+    }
 
-	/* Returns a regular expression describing the alphabet for this DFA,
-	 * eliminating extraneous characters. */
-	private String alphabetRegex() {
-		StringBuilder alphabet = new StringBuilder();
-		for (Character character : encodings.keySet()) {
-			alphabet.append("|" + character);
-		}
-		alphabet.replace(0, 1, "("); // Hacky fix to fence post issue.
-		alphabet.append(")*");
-		return alphabet.toString();
-	}
-	
-	/**
-	 * Exports this Builder's model as a Graphviz dot file and associated png.
-	 * 
-	 * @param filename
-	 *            the name of the dot file
-	 * @param exportFinal
-	 * 			  whether to export the final or initial model
-	 * @throws IOException
-	 */
-	public void exportDotAndPng(String filename, boolean exportFinal) throws IOException {
-		String dot = toGraphviz(exportFinal);
-		Writer output = new BufferedWriter(new FileWriter(new File(filename)));
-		try {
-			output.write(dot);
-		} finally {
-			output.close();
-		}
-		GraphExporter.generatePngFileFromDotFile(filename);
-	}
+    /* Intersects the given model with DFAs for all of the given invariants */
+    private Automaton intersectModelWith(Automaton model,
+            TemporalInvariantSet invariants) {
+        for (ITemporalInvariant invariant : invariants) {
+            char source = getEncoding(invariant.getFirst());
+            char dest = getEncoding(invariant.getSecond());
 
-	/* Constructs a Graphviz dot representation of the model. */
-	private String toGraphviz(boolean exportFinal) {
-		Automaton model = exportFinal ? finalModel : initialModel;
-		StringBuilder b = new StringBuilder("digraph {\n");
-		
-		// Assign states consecutive numbers.
-		Set<State> states = model.getStates();
-		Map<State, Integer> stateOrdering = new HashMap<State, Integer>();
-		int number = 0;
-		for (State s : states) {
-			stateOrdering.put(s, number++);
-		}
+            RegExp re = new RegExp(invariant.getRegex(source, dest));
+            model = BasicOperations.intersection(model, re.toAutomaton());
+        }
+        return model;
+    }
 
-		for (State s : states) {
-			b.append("  ").append(stateOrdering.get(s));
-			if (s.isAccept())
-				b.append(" [shape=doublecircle,label=\"\"];\n");
-			else
-				b.append(" [shape=circle,label=\"\"];\n");
-			if (s == initialModel.getInitialState()) {
-				b.append("  initial [shape=plaintext,label=\"\"];\n");
-				b.append("  initial -> ").append(stateOrdering.get(s))
-						.append("\n");
-			}
-			for (Transition t : s.getTransitions()) {
-				int source = stateOrdering.get(s);
-				int dest = stateOrdering.get(t.getDest());
-				char cur = t.getMin();
-				appendTransition(b, encodings.get(cur), source, dest);
-				while (cur < t.getMax()) {
-					cur++;
-					appendTransition(b, encodings.get(cur), source, dest);
-				}
-			}
-		}
-		return b.append("}\n").toString();
-	}
+    /*
+     * Returns a regular expression describing the INITIAL and TERMINAL
+     * constraints for the DFA.
+     */
+    private String initialAndTerminalRegex() {
+        return INITIAL + "[^" + INITIAL + TERMINAL + "]*" + TERMINAL;
+    }
 
-	/* Add a transition line to the given StringBuilder with the given label, from the source
-	 * to the dest nodes. */
-	private void appendTransition(StringBuilder b, String label, int source,
-			int dest) {
-		b.append("  ").append(source);
-		b.append(" -> ").append(dest).append(" [label=\"");
-		b.append(label);
-		b.append("\"]\n");
-	}
+    /*
+     * Returns a regular expression describing the alphabet for this DFA,
+     * eliminating extraneous characters.
+     */
+    private String alphabetRegex() {
+        StringBuilder alphabet = new StringBuilder();
+        for (Character character : encodings.keySet()) {
+            alphabet.append("|" + character);
+        }
+        alphabet.replace(0, 1, "("); // Hacky fix to fence post issue.
+        alphabet.append(")*");
+        return alphabet.toString();
+    }
+
+    /**
+     * Exports this Builder's model as a Graphviz dot file and associated png.
+     * 
+     * @param filename
+     *            the name of the dot file
+     * @param exportFinal
+     *            whether to export the final or initial model
+     * @throws IOException
+     */
+    public void exportDotAndPng(String filename, boolean exportFinal)
+            throws IOException {
+        String dot = toGraphviz(exportFinal);
+        Writer output = new BufferedWriter(new FileWriter(new File(filename)));
+        try {
+            output.write(dot);
+        } finally {
+            output.close();
+        }
+        GraphExporter.generatePngFileFromDotFile(filename);
+    }
+
+    /* Constructs a Graphviz dot representation of the model. */
+    private String toGraphviz(boolean exportFinal) {
+        Automaton model = exportFinal ? finalModel : initialModel;
+        StringBuilder b = new StringBuilder("digraph {\n");
+
+        // Assign states consecutive numbers.
+        Set<State> states = model.getStates();
+        Map<State, Integer> stateOrdering = new HashMap<State, Integer>();
+        int number = 0;
+        for (State s : states) {
+            stateOrdering.put(s, number++);
+        }
+
+        for (State s : states) {
+            b.append("  ").append(stateOrdering.get(s));
+            if (s.isAccept())
+                b.append(" [shape=doublecircle,label=\"\"];\n");
+            else
+                b.append(" [shape=circle,label=\"\"];\n");
+            if (s == initialModel.getInitialState()) {
+                b.append("  initial [shape=plaintext,label=\"\"];\n");
+                b.append("  initial -> ").append(stateOrdering.get(s))
+                        .append("\n");
+            }
+            for (Transition t : s.getTransitions()) {
+                int source = stateOrdering.get(s);
+                int dest = stateOrdering.get(t.getDest());
+                char cur = t.getMin();
+                appendTransition(b, encodings.get(cur), source, dest);
+                while (cur < t.getMax()) {
+                    cur++;
+                    appendTransition(b, encodings.get(cur), source, dest);
+                }
+            }
+        }
+        return b.append("}\n").toString();
+    }
+
+    /*
+     * Add a transition line to the given StringBuilder with the given label,
+     * from the source to the dest nodes.
+     */
+    private void appendTransition(StringBuilder b, String label, int source,
+            int dest) {
+        b.append("  ").append(source);
+        b.append(" -> ").append(dest).append(" [label=\"");
+        b.append(label);
+        b.append("\"]\n");
+    }
 }
