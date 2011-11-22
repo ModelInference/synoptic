7a2eda93325992fa69010e35570f111cb26591a9
Jenny Abrahamson <jabrah@cs.washington.edu>
2011-11-22 01:34:41 -0800

    Addressed code review on revision 971ce006cb24

--- InvDFAMinimization/src/model/InvModel.java
+++ InvDFAMinimization/src/model/InvModel.java
@@ -1,24 +1,28 @@
 package model;
 
 import synoptic.invariants.ITemporalInvariant;
-import synoptic.invariants.TOInitialTerminalInvariant;
-import synoptic.main.TraceParser;
-import synoptic.model.EventType;
 
+/**
+ * Extends the EncodedAutomaton class to encode a single ITemporalInvariant.
+ * 
+ * @author Jenny
+ *
+ */
 public class InvModel extends EncodedAutomaton {
 	
+    // The invariant represented with this Automaton.
 	private ITemporalInvariant inv;
 	
+	/**
+	 * Generates an EncodedAutomaton for the given invariant. Encodes the names of both
+	 * EventTypes composing the invariant and constructs the Automaton by using those characters
+	 * in a regex representing the invariant.
+	 */
 	public InvModel(ITemporalInvariant invariant) {
 		this.inv = invariant;
 		char first = super.getEncoding(inv.getFirst().toString());
 		char second = super.getEncoding(inv.getSecond().toString());
 		String re = inv.getRegex(first, second);
 		super.intersectWithRE(re);
-	}
-	
-	public static InvModel initialTerminalInv(EventType initial, EventType terminal) {
-		return new InvModel(new TOInitialTerminalInvariant(initial, 
-				terminal, TraceParser.defaultRelation));
 	}
 }
