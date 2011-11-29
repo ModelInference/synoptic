b3c38cb913274a2e18bcfaaa5e2a8d366905e91a
Jenny Abrahamson <jabrah@cs.washington.edu>
2011-11-28 16:10:19 -0800

    Initial version: translation from Synoptic model to dfa

--- InvDFAMinimization/src/main/DFAMain.java
+++ InvDFAMinimization/src/main/DFAMain.java
@@ -1,8 +1,10 @@
 package main;
 
-import plume.Option;
 import model.InvModel;
 import model.InvsModel;
+import model.SynopticModel;
+
+import plume.Option;
 
 import synoptic.invariants.ITemporalInvariant;
 import synoptic.invariants.TOInitialTerminalInvariant;
@@ -14,10 +16,11 @@ import synoptic.model.PartitionGraph;
 import synoptic.model.StringEventType;
 
 /**
- * InvDFAMinimization accepts a log file and regular expression arguments and uses Synoptic to
- * parse the log and mine invariants. Implicit NIFby invariants along with an Initial/Terminal
- * invariant are used to construct and export an initial dfa model. Synoptic's mined invariants
- * are then intersected with the initial model to construct and export a final model.
+ * InvDFAMinimization accepts a log file and regular expression arguments and
+ * uses Synoptic to parse the log and mine invariants. Implicit NIFby invariants
+ * along with an Initial/Terminal invariant are used to construct and export an
+ * initial dfa model. Synoptic's mined invariants are then intersected with the
+ * initial model to construct and export a final model.
  * 
  * @author Jenny
  */
@@ -29,67 +32,77 @@ public class DFAMain {
     @Option(value = "-i Initial model output filename",
             aliases = { "-initialModelFile" })
     public static String initialModelFile = "initialDfaModel.dot";
-    
+
     /**
      * Final model output filename.
      */
     @Option(value = "-f Final model output filename",
             aliases = { "-finalModelFile" })
     public static String finalModelFile = "finalDfaModel.dot";
-    
-	public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws Exception {
         Main synMain = Main.processArgs(args);
         PartitionGraph initialModel = synMain.createInitialPartitionGraph();
-        
+
         // Construct initial dfa from NIFby invariants.
         TemporalInvariantSet NIFbys = initialModel.getNIFbyInvariants();
         InvsModel dfa = getMinModelFromInvs(NIFbys);
-        
+
         // Intersect with initial/terminal InvModel.
-        /* 
-         * TODO: Replace once getInitial and getTerminal are implemented (Issue 173)
-         * EventType initial = initialModel.getInitialEvent();
-         * EventType terminal = initialModel.getTerminalEvent();
+        /*
+         * TODO: Replace once getInitial and getTerminal are implemented (Issue
+         * 173) EventType initial = initialModel.getInitialEvent(); EventType
+         * terminal = initialModel.getTerminalEvent();
          */
         EventType initial = StringEventType.newInitialStringEventType();
         EventType terminal = StringEventType.newTerminalStringEventType();
-        InvModel initialTerminalInv = new InvModel(new TOInitialTerminalInvariant(initial, 
-				terminal, TraceParser.defaultRelation));
-		dfa.intersectWith(initialTerminalInv);
-		
-		// Export initial model.
-		dfa.exportDotAndPng(initialModelFile);
-		
-		// Intersect with mined invariants.
+        InvModel initialTerminalInv = new InvModel(
+                new TOInitialTerminalInvariant(initial, terminal,
+                        TraceParser.defaultRelation));
+        dfa.intersectWith(initialTerminalInv);
+
+        // Export initial model.
+        dfa.exportDotAndPng(initialModelFile);
+
+        // Intersect with mined invariants.
         TemporalInvariantSet minedInvariants = initialModel.getInvariants();
-		dfa.intersectWith(getMinModelFromInvs(minedInvariants));
-		
-		// Minimize the model.
-		dfa.minimize();
-		
-		// Export final model.
+        dfa.intersectWith(getMinModelFromInvs(minedInvariants));
+
+        // Minimize the model.
+        dfa.minimize();
+
+        // Export final model.
         dfa.exportDotAndPng(finalModelFile);
-	}
-	
-	/*
-     * Constructs an InvsModel by intersecting InvModels for each of the given temporal invariants.
+
+        synMain.runSynoptic(initialModel);
+        SynopticModel converted = new SynopticModel(initialModel);
+        converted.exportDotAndPng("convertedModelpreMin");
+        converted.minimize();
+        converted.exportDotAndPng("convertedModelpostMin");
+    }
+
+    /*
+     * Constructs an InvsModel by intersecting InvModels for each of the given
+     * temporal invariants.
+     * 
      * @param invariants a set of TemporalInvariants
+     * 
      * @return the intersected InvsModel
      */
     public static InvsModel getMinModelFromInvs(TemporalInvariantSet invariants) {
 
         // Initial model will accept all Strings.
         InvsModel model = new InvsModel();
-        
+
         // Intersect provided invariants.
         for (ITemporalInvariant invariant : invariants) {
             InvModel current = new InvModel(invariant);
             model.intersectWith(current);
         }
-        
+
         // Optimize by minimizing the model.
         model.minimize();
-        
+
         return model;
     }
 }
--- InvDFAMinimization/src/model/InvModel.java
+++ InvDFAMinimization/src/model/InvModel.java
@@ -6,23 +6,22 @@ import synoptic.invariants.ITemporalInvariant;
  * Extends the EncodedAutomaton class to encode a single ITemporalInvariant.
  * 
  * @author Jenny
- *
  */
 public class InvModel extends EncodedAutomaton {
-	
+
     // The invariant represented with this Automaton.
-	private ITemporalInvariant inv;
-	
-	/**
-	 * Generates an EncodedAutomaton for the given invariant. Encodes the names of both
-	 * EventTypes composing the invariant and constructs the Automaton by using those characters
-	 * in a regex representing the invariant.
-	 */
-	public InvModel(ITemporalInvariant invariant) {
-		this.inv = invariant;
-		char first = super.getEncoding(inv.getFirst().toString());
-		char second = super.getEncoding(inv.getSecond().toString());
-		String re = inv.getRegex(first, second);
-		super.intersectWithRE(re);
-	}
+    private ITemporalInvariant inv;
+
+    /**
+     * Generates an EncodedAutomaton for the given invariant. Encodes the names
+     * of both EventTypes composing the invariant and constructs the Automaton
+     * by using those characters in a regex representing the invariant.
+     */
+    public InvModel(ITemporalInvariant invariant) {
+        this.inv = invariant;
+        char first = super.getEncoding(inv.getFirst().toString());
+        char second = super.getEncoding(inv.getSecond().toString());
+        String re = inv.getRegex(first, second);
+        super.intersectWithRE(re);
+    }
 }
