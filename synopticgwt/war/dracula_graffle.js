e07d5a73d0fb2cd9ed01550f3ff1048d22852cb2
kevin.a.thai <kevin.a.thai@localhost>
2011-12-04 23:44:21 -0800

    Split up the array containing potential input/output points for objects into an array for potential "input" points and an array for potential "output" points. Modified code regarding coordinate calculations to work with the two separate arrays. Graph is visually unchanged.

--- synopticgwt/war/dracula_graffle.js
+++ synopticgwt/war/dracula_graffle.js
@@ -72,17 +72,33 @@ Raphael.fn.connection = function (obj1, obj2, style) {
                 var off1 = 0;
                 var off2 = 0;
                 /* coordinates for potential connection coordinates from/to the objects */
-                var p = [
+               // var p = [
+                 //   {x: bb1.x + bb1.width / 2, y: bb1.y - off1},              /* NORTH 1 */
+                   // {x: bb1.x + bb1.width / 2, y: bb1.y + bb1.height + off1}, /* SOUTH 1 */
+                   // {x: bb1.x - off1, y: bb1.y + bb1.height / 2},             /* WEST  1 */
+                   // {x: bb1.x + bb1.width + off1, y: bb1.y + bb1.height / 2}, /* EAST  1 */
+                   // {x: bb2.x + bb2.width / 2, y: bb2.y - off2},              /* NORTH 2 */
+                    //{x: bb2.x + bb2.width / 2, y: bb2.y + bb2.height + off2}, /* SOUTH 2 */
+                    //{x: bb2.x - off2, y: bb2.y + bb2.height / 2},             /* WEST  2 */
+                   // {x: bb2.x + bb2.width + off2, y: bb2.y + bb2.height / 2}  /* EAST  2 */
+                //];
+                
+                /* coordinates for potential connection coordinates from bb1 */
+                var p_inputs = [
                     {x: bb1.x + bb1.width / 2, y: bb1.y - off1},              /* NORTH 1 */
-                    {x: bb1.x + bb1.width / 2, y: bb1.y + bb1.height + off1}, /* SOUTH 1 */
-                    {x: bb1.x - off1, y: bb1.y + bb1.height / 2},             /* WEST  1 */
-                    {x: bb1.x + bb1.width + off1, y: bb1.y + bb1.height / 2}, /* EAST  1 */
-                    {x: bb2.x + bb2.width / 2, y: bb2.y - off2},              /* NORTH 2 */
-                    {x: bb2.x + bb2.width / 2, y: bb2.y + bb2.height + off2}, /* SOUTH 2 */
-                    {x: bb2.x - off2, y: bb2.y + bb2.height / 2},             /* WEST  2 */
-                    {x: bb2.x + bb2.width + off2, y: bb2.y + bb2.height / 2}  /* EAST  2 */
+                	{x: bb1.x + bb1.width / 2, y: bb1.y + bb1.height + off1}, /* SOUTH 1 */
+                	{x: bb1.x - off1, y: bb1.y + bb1.height / 2},             /* WEST  1 */
+                	{x: bb1.x + bb1.width + off1, y: bb1.y + bb1.height / 2}, /* EAST  1 */       
                 ];
-
+                             
+                /* coordinates for potential connection coordinates to bb2 */
+                var p_outputs = [
+                 {x: bb2.x + bb2.width / 2, y: bb2.y - off2},              /* NORTH 2 */
+                 {x: bb2.x + bb2.width / 2, y: bb2.y + bb2.height + off2}, /* SOUTH 2 */
+                 {x: bb2.x - off2, y: bb2.y + bb2.height / 2},             /* WEST  2 */
+                 {x: bb2.x + bb2.width + off2, y: bb2.y + bb2.height / 2}  /* EAST  2 */
+                 ];
+                                
                 /* distances between objects and according coordinates connection */
                 var d = {}, dis = [];
 
@@ -91,28 +107,30 @@ Raphael.fn.connection = function (obj1, obj2, style) {
                  */
                 /* loop the first object's connection coordinates */
                 for (var i = 0; i < 4; i++) {
-                    /* loop the seond object's connection coordinates */
-                    for (var j = 4; j < 8; j++) {
-                        var dx = Math.abs(p[i].x - p[j].x),
-                            dy = Math.abs(p[i].y - p[j].y);
-                        if ((i == j - 4) || (((i != 3 && j != 6) || p[i].x < p[j].x) && ((i != 2 && j != 7) || p[i].x > p[j].x) && ((i != 0 && j != 5) || p[i].y > p[j].y) && ((i != 1 && j != 4) || p[i].y < p[j].y))) {
-                            dis.push(dx + dy);
-                            d[dis[dis.length - 1].toFixed(3)] = [i, j];
-                        }
-                    }
+                	/* loop the second object's connection coordinates */
+                	for (var j = 0; j < 4; j++) {
+                		var dx = Math.abs(p_inputs[i].x - p_outputs[j].x),
+                			dy = Math.abs(p_inputs[i].y - p_outputs[j].y);
+                		if ((i == j) || (((i != 3 && j != 3) || p_inputs[i].x < p_outputs[j].x) && ((i != 2 && j != 3) || p_inputs[i].x > p_outputs[j].x) && ((i != 0 && j != 1) || p_inputs[i].y > p_outputs[j].y) && ((i != 1 && j != 0) || p_inputs[i].y < p_outputs[j].y))) {
+                			dis.push(dx + dy);
+                			d[dis[dis.length - 1].toFixed(3)] = [i, j + 4];
+                		}
+                	}
                 }
-                var res = dis.length == 0 ? [0, 4] : d[Math.min.apply(Math, dis).toFixed(3)];
+                
+                var disIsEmpty = dis.length == 0;
+                var res = disIsEmpty ? [0, 4] : d[Math.min.apply(Math, dis).toFixed(3)];
                 /* bezier path */
-                var x1 = p[res[0]].x,
-                    y1 = p[res[0]].y,
-                    x4 = p[res[1]].x,
-                    y4 = p[res[1]].y,
-                    dx = Math.max(Math.abs(x1 - x4) / 2, 10),
-                    dy = Math.max(Math.abs(y1 - y4) / 2, 10),
-                    x2 = [x1, x1, x1 - dx, x1 + dx][res[0]].toFixed(3),
-                    y2 = [y1 - dy, y1 + dy, y1, y1][res[0]].toFixed(3),
-                    x3 = [0, 0, 0, 0, x4, x4, x4 - dx, x4 + dx][res[1]].toFixed(3),
-                    y3 = [0, 0, 0, 0, y1 + dy, y1 - dy, y4, y4][res[1]].toFixed(3);
+                var x1 = p_inputs[res[0]].x,
+                	y1 = p_inputs[res[0]].y,
+                	x4 = p_outputs[disIsEmpty ? res[1] : res[1] - 4].x,
+                	y4 = p_outputs[disIsEmpty ? res[1] : res[1] - 4].y,
+                	dx = Math.max(Math.abs(x1 - x4) / 2, 10),
+                	dy = Math.max(Math.abs(y1 - y4) / 2, 10),
+                	x2 = [x1, x1, x1 - dx, x1 + dx][res[0]].toFixed(3),
+                	y2 = [y1 - dy, y1 + dy, y1, y1][res[0]].toFixed(3),  
+                	x3 = [0, 0, 0, 0, x4, x4, x4 - dx, x4 + dx][res[1]].toFixed(3),
+                	y3 = [0, 0, 0, 0, y1 + dy, y1 - dy, y4, y4][res[1]].toFixed(3);
 
                 /* assemble path and arrow */
                 path = ["M", x1.toFixed(3), y1.toFixed(3), "C", x2, y2, x3, y3, x4.toFixed(3), y4.toFixed(3)].join(",");
