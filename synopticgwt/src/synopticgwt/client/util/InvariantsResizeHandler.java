68449c6b11e88dd4c4b603644e11eb51ed96d038
T.101.JV <T.101.JV@localhost>
2012-01-01 20:57:00 -0800

    Added inv resize handler and a few other changes

---
+++ synopticgwt/src/synopticgwt/client/util/InvariantsResizeHandler.java
@@ -0,0 +1,32 @@
+package synopticgwt.client.util;
+
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.user.client.ui.TabBar;
+
+import synopticgwt.client.SynopticGWT;
+import synopticgwt.client.invariants.InvariantsTab;
+
+/**
+ * A resize handler that takes care of updating the model graphic whenever the
+ * model tab is resized.
+ */
+public class InvariantsResizeHandler implements ResizeHandler {
+    
+    private TabBar tabBar;
+    private InvariantsTab invTab;
+    
+    public InvariantsResizeHandler(TabBar tabBar, InvariantsTab invTab) {
+        super();
+        this.tabBar = tabBar;
+        this.invTab = invTab;
+    }
+
+    @Override
+    public void onResize(ResizeEvent event) {
+        if (tabBar.isTabEnabled(SynopticGWT.invariantsTabIndex)) {
+            invTab.resize();
+        }
+    }
+
+}
