ba490df9a0986da2e25f78b7a23d5cd32d8128de
a.w.davies.vio <a.w.davies.vio@localhost>
2012-01-31 00:24:58 -0800

    Added RadioButton class to handle overlaying a given path on the model.

---
+++ synopticgwt/src/synopticgwt/client/model/PathDisplayRadioButton.java
@@ -0,0 +1,28 @@
+package synopticgwt.client.model;
+
+import java.util.Set;
+
+import synopticgwt.shared.GWTEdge;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.RadioButton;
+
+public class PathDisplayRadioButton extends RadioButton {
+
+    private final Set<GWTEdge> path;
+    
+    public PathDisplayRadioButton(String group, String traceID,
+            Set<GWTEdge> path) {
+        super(group, traceID);
+        this.path = path;
+        this.addClickHandler(new PDRBClickHandler());
+    }
+    
+    private class PDRBClickHandler implements ClickHandler {
+        
+        public void onClick(ClickEvent event) {
+            // TODO: Overlay path on the model.
+        }
+    }
+}
