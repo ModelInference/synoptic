d1ca592bb180556f91c5f60db82aa908b04dc8e2
Ivan Beschastnikh <ivan@cs.washington.edu>
2012-02-03 07:13:30 -0800

    Fixed failing test in InvDFAMin, expanded tests, made the encoding code more robust, and fixed some latent bugs.

--- InvDFAMinimization/src/tests/units/EncodingTests.java
+++ InvDFAMinimization/src/tests/units/EncodingTests.java
@@ -1,6 +1,7 @@
 package tests.units;
 
-import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 
 import java.util.HashSet;
@@ -16,6 +17,7 @@ import synoptic.model.StringEventType;
 public class EncodingTests {
 
     public static final EventType aEvent = new StringEventType("a");
+
     public static final EventType bEvent = new StringEventType("b");
     public static final EventType cEvent = new StringEventType("c");
     public static final EventType dEvent = new StringEventType("d");
@@ -35,15 +37,38 @@ public class EncodingTests {
 
     @Test
     public void testGetEncoding() {
+        final EventType a2Event = new StringEventType("a");
+        final EventType xEvent = new StringEventType("x");
+        final EventType yEvent = new StringEventType("y");
+        final EventType z2Event = new StringEventType("z");
+
         EventTypeEncodings encodings = getBasicEncodings();
+        // Check one event type that has been encoded.
+        assertEquals(encodings.getEncoding(aEvent),
+                encodings.getEncoding(aEvent));
 
-        assertSame(encodings.getEncoding(aEvent), encodings.getEncoding(aEvent));
+        // Check an encoded event type and a NON-encoded event type. These are
+        // different event type objects, but have the same string
+        // representations.
+        assertEquals(encodings.getEncoding(aEvent),
+                encodings.getEncoding(a2Event));
 
-        assertNotSame(encodings.getEncoding(aEvent),
-                encodings.getEncoding(bEvent));
+        // Check two different event types that have been encoded.
+        assertFalse(((Character) encodings.getEncoding(aEvent))
+                .equals(encodings.getEncoding(bEvent)));
 
-        assertNotSame(encodings.getEncoding(zEvent),
-                encodings.getEncoding(zEvent));
+        // Check one event type that has NOT been encoded.
+        assertEquals(encodings.getEncoding(dEvent),
+                encodings.getEncoding(dEvent));
+
+        // Check two event types that have NOT been encoded.
+        assertFalse(((Character) encodings.getEncoding(xEvent))
+                .equals(encodings.getEncoding(yEvent)));
+
+        // Check encoding equality of two NON-encoded events that are different
+        // event type objects, but have the same string representation.
+        assertEquals(encodings.getEncoding(zEvent),
+                encodings.getEncoding(z2Event));
     }
 
     @Test
