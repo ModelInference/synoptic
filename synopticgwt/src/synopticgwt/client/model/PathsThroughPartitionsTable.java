397c911c1b75ae34483a1862a6822fab0d137cb5
a.w.davies.vio <a.w.davies.vio@localhost>
2012-02-12 22:47:18 -0800

    Added labels to paths.  When clicking on a radio button, the trace IDs associated with the path are shown, but cannot be cleared yet.

--- synopticgwt/src/synopticgwt/client/model/PathsThroughPartitionsTable.java
+++ synopticgwt/src/synopticgwt/client/model/PathsThroughPartitionsTable.java
@@ -1,14 +1,12 @@
 package synopticgwt.client.model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import synopticgwt.shared.GWTEdge;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.FlexTable;
 
 /**
@@ -22,6 +20,9 @@ public class PathsThroughPartitionsTable extends FlexTable {
 
     // The group under which to put all radio buttons.
     private static final String RADIO_BUTTON_GROUP = "traceRadioButton";
+
+    // Set whenever "showPaths" method is called.
+    private Map<Set<GWTEdge>, Set<Integer>> paths;
 
     /**
      * Clears the table of any displayed paths.
@@ -45,16 +46,56 @@ public class PathsThroughPartitionsTable extends FlexTable {
      */
     public void showPaths(Map<Set<GWTEdge>, Set<Integer>> paths) {
         this.clearPaths();
+        this.paths = paths;
         int row = 0;
 
         Set<Set<GWTEdge>> keys = paths.keySet();
 
+        PathButtonClickHandler clickHandler = new PathButtonClickHandler();
+
         // Create a set of radio buttons related to each path.
         for (Set<GWTEdge> path : keys) {
             PathDisplayRadioButton button = new PathDisplayRadioButton(
-                    RADIO_BUTTON_GROUP, paths.get(path), path);
+                    RADIO_BUTTON_GROUP, "Path " + (row + 1), paths.get(path),
+                    path);
+            button.addClickHandler(clickHandler);
             this.setWidget(row, 0, button);
             row++;
+        }
+    }
+
+    /**
+     * A click handler that inserts all of the trace IDs for the corresponding
+     * path displaying radio button. Any other trace IDs listed prior will be
+     * removed.
+     */
+    private class PathButtonClickHandler implements ClickHandler {
+        public void onClick(ClickEvent event) {
+            PathDisplayRadioButton button = (PathDisplayRadioButton) event
+                    .getSource();
+
+            // TODO Clear the last table shown when a new button has been
+            // clicked.
+
+            // Create a table showing all of the traces to be inserted
+            // after the button that has been clicked.
+            // Make sure to sort it for readability.
+            FlexTable traceIDsTable = new FlexTable();
+            Integer[] traceIDs = new Integer[paths.get(button.getPath())
+                    .size()];
+            paths.get(button.getPath()).toArray(traceIDs);
+            int row = 0;
+            for (Integer traceID : traceIDs) {
+                traceIDsTable.setText(row, 0, "Trace " + traceID);
+                row++;
+            }
+
+            // Find the row in which the event took place on the table
+            // (the one housing this class), and then insert the created
+            // table right after the button.
+            int rowSource = getCellForEvent(event).getRowIndex() + 1;
+            insertRow(rowSource);
+            setWidget(rowSource, 1, traceIDsTable);
         }
     }
 }
