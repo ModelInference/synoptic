55c3511e0789bf4b29ea138699733f2a3d21ec47
kevin.a.thai <kevin.a.thai@localhost>
2012-02-26 04:23:03 -0800

    Minor modifications to Derby test class.

--- synopticgwt/test/synopticgwt/server/DerbyDBTests.java
+++ synopticgwt/test/synopticgwt/server/DerbyDBTests.java
@@ -1,24 +1,23 @@
 package synopticgwt.server;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 /**
  * Test an instance of a Derby instance.
  */
 public class DerbyDBTests {
+    /** Database name and path */
     private static String path = "/Users/Kevin/Desktop/DerbyUnitTest";
-    
-    private DerbyDB db;
-    
+        
     /**
      * Create a new db and table. Writes to the table. Check that read data is
      * the same as written data.
+     * Note: To use test, make sure database name/path doesn't already exist.
      */
     @Test
     public void testNewDatabase() {
-        db = DerbyDB.getInstance(path, true);
+        DerbyDB db = DerbyDB.getInstance(path, true);
         String createNewTable = "CREATE TABLE Test (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), text VARCHAR(15))";
         db.createQuery(createNewTable);
         assertNotNull(db);
