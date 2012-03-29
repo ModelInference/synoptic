ecfc6577d3bf90f6941d20d6c7f45e12f840dea9
Ivan Beschastnikh <ivan@cs.washington.edu>
2012-03-29 15:44:03 -0700

    Updated InvariMint with minor changes.

--- InvariMint/src/main/InvariMintMain.java
+++ InvariMint/src/main/InvariMintMain.java
@@ -28,6 +28,7 @@ import synoptic.main.Main;
 import synoptic.main.SynopticOptions;
 import synoptic.main.TraceParser;
 import synoptic.model.ChainsTraceGraph;
+import synoptic.model.Event;
 import synoptic.model.EventNode;
 import synoptic.model.EventType;
 import synoptic.model.PartitionGraph;
@@ -195,7 +196,7 @@ public class InvariMintMain {
         EventType terminalEvent = StringEventType.newTerminalStringEventType();
         InvModel initialTerminalInv = new InvModel(
                 new TOInitialTerminalInvariant(initialEvent, terminalEvent,
-                        TraceParser.defaultRelation), encodings);
+                        Event.defaultTimeRelationString), encodings);
         dfa.intersectWith(initialTerminalInv);
 
         // Intersect with mined invariants.
@@ -222,7 +223,8 @@ public class InvariMintMain {
             EventType initialEvent, EventType terminalEvent) {
 
         Map<Pair, Set<Character>> seenTransitions = new HashMap<Pair, Set<Character>>();
-        EventNode initNode = g.getDummyInitialNode(TraceParser.defaultRelation);
+        EventNode initNode = g
+                .getDummyInitialNode(Event.defaultTimeRelationString);
 
         // Iterate through all the traces -- each transition from the INITIAL
         // node connects\holds a single trace.
