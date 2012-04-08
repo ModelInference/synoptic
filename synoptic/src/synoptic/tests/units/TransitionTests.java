2fb0a3a8acdc783fe6bf2b6442d8f76331653c2c
a.w.davies.vio <a.w.davies.vio@localhost>
2012-04-07 21:23:06 -0700

    Updated comments.  Added computeMean to ITimesSeries.  Fixed up most tests.

--- synoptic/src/synoptic/tests/units/TransitionTests.java
+++ synoptic/src/synoptic/tests/units/TransitionTests.java
@@ -5,69 +5,67 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import synoptic.model.EventNode;
-import synoptic.model.StringEventType;
-import synoptic.model.Transition;
+import synoptic.util.time.ITimeSeries;
 import synoptic.util.time.ITotalTime;
 
 public class TransitionTests {
 
-    private Transition<Integer> trans;
+    private ITimeSeries<ITotalTime> times;
 
     @Before
-    public void resetTrans() {
-        trans = new Transition(1, 2, "");
+    public void resettimes() {
+        times = new ITimeSeries();
     }
 
     // Does it compute the mode with only one value?
     @Test
     public void modeDeltaTestOneValue() {
-        trans.addDelta(new ITotalTime(1));
-        assertEquals(new ITotalTime(1), trans.computeModeDelta());
+        times.addDelta(new ITotalTime(1));
+        assertEquals(new ITotalTime(1), times.computeMode());
     }
 
     // Does it compute the mode with no values?
     @Test
     public void modeDeltaTestEmpty() {
-        assertNull(trans.computeModeDelta());
+        assertNull(times.computeMode());
     }
 
     // Does it compute the mode with many arbitrary values
     // (one is the most used).
     @Test
     public void modeDeltaTestManyValues() {
-        trans.addDelta(new ITotalTime(1));
-        trans.addDelta(new ITotalTime(8));
-        trans.addDelta(new ITotalTime(2));
-        trans.addDelta(new ITotalTime(1));
-        trans.addDelta(new ITotalTime(8));
-        trans.addDelta(new ITotalTime(1));
-        trans.addDelta(new ITotalTime(16));
-        trans.addDelta(new ITotalTime(16));
-        trans.addDelta(new ITotalTime(1));
-        trans.addDelta(new ITotalTime(8));
-        trans.addDelta(new ITotalTime(1));
-        assertEquals(new ITotalTime(1), trans.computeModeDelta());
+        times.addDelta(new ITotalTime(1));
+        times.addDelta(new ITotalTime(8));
+        times.addDelta(new ITotalTime(2));
+        times.addDelta(new ITotalTime(1));
+        times.addDelta(new ITotalTime(8));
+        times.addDelta(new ITotalTime(1));
+        times.addDelta(new ITotalTime(16));
+        times.addDelta(new ITotalTime(16));
+        times.addDelta(new ITotalTime(1));
+        times.addDelta(new ITotalTime(8));
+        times.addDelta(new ITotalTime(1));
+        assertEquals(new ITotalTime(1), times.computeMode());
     }
 
     // Does the median work with one value?
     @Test
     public void medianTestOneValue() {
-        trans.addDelta(new ITotalTime(1));
-        assertEquals(new ITotalTime(1), trans.computeMedianDelta());
+        times.addDelta(new ITotalTime(1));
+        assertEquals(new ITotalTime(1), times.computeMedian());
     }
 
     @Test
     public void medianTestEmpty() {
-        assertNull(trans.computeMedianDelta());
+        assertNull(times.computeMedian());
     }
 
     @Test
     public void medianTestManyValues() {
         for (int i = 1; i <= 5; i++) {
-            trans.addDelta(new ITotalTime(i));
+            times.addDelta(new ITotalTime(i));
         }
 
-        assertEquals(new ITotalTime(3), trans.computeMedianDelta());
+        assertEquals(new ITotalTime(3), times.computeMedian());
     }
 }
