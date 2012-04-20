2d2fa8d63f63017f476ab61eaad031bb823be86a
Ivan Beschastnikh <ivan@cs.washington.edu>
2012-04-20 13:25:53 -0700

    Removed unused functional java util code.

--- synoptic/src/synoptic/util/Predicate.java
+++ synoptic/src/synoptic/util/Predicate.java
@@ -1,41 +1,24 @@
 package synoptic.util;
 
+/**
+ * Defines a few useful binary predicates.
+ */
 public class Predicate {
-    public static interface IBinary<A, B> {
+    public static interface IBoolBinary<A, B> {
         public boolean eval(A a, B b);
     }
 
-    public static class BinaryFalse<A, B> implements IBinary<A, B> {
+    /** Evaluates two objects and always returns false. */
+    public static class BinaryFalse<A, B> implements IBoolBinary<A, B> {
         public boolean eval(A a, B b) {
             return false;
         }
     }
 
-    public static class BinaryTrue<A, B> implements IBinary<A, B> {
+    /** Evaluates two objects and always returns true. */
+    public static class BinaryTrue<A, B> implements IBoolBinary<A, B> {
         public boolean eval(A a, B b) {
             return true;
-        }
-    }
-
-    public static class Equals<T> implements IBinary<T, T> {
-        public boolean eval(T a, T b) {
-            return a.equals(b);
-        }
-    }
-
-    public static Predicate.Equals<String> stringEquality = new Predicate.Equals<String>();
-
-    public static class ComposedFirst<A, B, C> implements IBinary<A, C> {
-        Function.IUnary<A, B> fa;
-        IBinary<B, C> fb;
-
-        public ComposedFirst(Function.IUnary<A, B> a, IBinary<B, C> b) {
-            fa = a;
-            fb = b;
-        }
-
-        public boolean eval(A a, C b) {
-            return fb.eval(fa.eval(a), b);
         }
     }
 }
