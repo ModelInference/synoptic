087d1d213265fae8cbe29736c3809d3d4d7a2d0c
Jenny Abrahamson <jabrah@cs.washington.edu>
2012-04-23 12:17:26 -0700

    End to end test with oracle for InvariMint Ktails

--- InvariMint/src/tests/integration/EndToEndKTailsTest.java
+++ InvariMint/src/tests/integration/EndToEndKTailsTest.java
@@ -1,12 +1,20 @@
 package tests.integration;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import main.InvariMintMain;
+import main.InvariMintOptions;
+import model.EncodedAutomaton;
 
 import org.junit.Test;
 
-import synoptic.main.SynopticMain;
+import synoptic.model.event.EventType;
+import synoptic.model.event.StringEventType;
 
 import tests.InvariMintTest;
 
@@ -22,10 +30,29 @@ public class EndToEndKTailsTest extends InvariMintTest {
                 + "abstract" + File.separator + "ktails-test" + File.separator;
 
         String[] args = new String[] { "--performKTails=true", "--kTailLength",
-                "2", "-r", "^(?<DTIME>)(?<nodename>)(?<TYPE>)$", "-m",
-                "\\k<nodename>", "-o", testOutputDir + "ktails-example",
-                tPath + "trace.txt" };
-        SynopticMain.instance = null;
-        InvariMintMain.main(args);
+                "2", "--runSynoptic=false", "-r",
+                "^(?<DTIME>)(?<nodename>)(?<TYPE>)$", "-m", "\\k<nodename>",
+                "-o", testOutputDir + "ktails-example", tPath + "trace.txt" };
+
+        InvariMintOptions opts = new InvariMintOptions(args);
+        EncodedAutomaton dfa = InvariMintMain.runInvariMint(opts);
+
+        List<EventType> sequence = new ArrayList<EventType>();
+        sequence.add(new StringEventType("INITIAL"));
+        sequence.add(new StringEventType("x"));
+        sequence.add(new StringEventType("y"));
+        sequence.add(new StringEventType("z"));
+        sequence.add(new StringEventType("d"));
+        sequence.add(new StringEventType("TERMINAL"));
+        assertTrue(dfa.run(sequence));
+
+        sequence.remove(4);
+        sequence.add(4, new StringEventType("c"));
+        assertTrue(dfa.run(sequence));
+
+        sequence.remove(4);
+        sequence.add(4, new StringEventType("f"));
+        assertFalse(dfa.run(sequence));
+
     }
 }
