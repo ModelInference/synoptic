fc1332a12dd04cf23b6fbab8f99700421583034c
Andrew Davies <a.w.davies.vio@gmail.com>
2012-04-24 12:48:25 -0700

    Added definition for constrained temporal invariants.

--- synoptic/src/synoptic/invariants/constraints/IThresholdConstraint.java
+++ synoptic/src/synoptic/invariants/constraints/IThresholdConstraint.java
@@ -4,6 +4,17 @@ import synoptic.util.time.ITime;
 
 /**
  * Represents a time threshold constraint on some temporal invariant.
+ *
+ * A constrained invariant is defined as follows:
+ * <pre>
+ *  Whenever an event a_i is encountered in trace t:
+ *
+ *      1. a_i is followed by at least one c_i before TERMINAL.
+ *
+ *      2. Either one of the following is true:
+ *          a.) The first c_i, c_j, follows a_i in at most n time.
+ *          b.) The first c_i, c_j, follows a_i in at leas n time.
+ * </pre>
  */
 public interface IThresholdConstraint {
     /**
