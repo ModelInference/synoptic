3d74b3d91430f5b6e3e1ece1b08f528ea3ccd0b4
T.101.JV <T.101.JV@localhost>
2012-04-27 22:32:18 -0700

    Modified the BiRelational type hierarchy, wrote docs, and impelmented shorten

--- synoptic/src/synoptic/invariants/APBiRelationInvariant.java
+++ synoptic/src/synoptic/invariants/APBiRelationInvariant.java
@@ -6,17 +6,15 @@ import synoptic.model.event.EventType;
 import synoptic.model.interfaces.INode;
 
 /**
- * TODO: describe.
+ * Represents a birelational event invariant in the input 
+ * traces where the first event is always preceded by the 
+ * second event.
  */
-public class APBiRelationInvariant extends BinaryInvariant implements
-        IBiRelationalInvariant {
-
-    protected String orderingRelation;
+public class APBiRelationInvariant extends BiRelationalInvariant {
 
     public APBiRelationInvariant(EventType first, EventType second,
             String relation, String orderingRelation) {
-        super(first, second, relation);
-        this.orderingRelation = orderingRelation;
+        super(first, second, relation, orderingRelation);
     }
 
     @Override
@@ -24,10 +22,27 @@ public class APBiRelationInvariant extends BinaryInvariant implements
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Returns a sub-trace of the violating trace that looks like ...'second'
+     * where 'first' APby 'second' is this invariant and where 'first' does
+     * _not_ appear in the returned sub-trace at all. The returned sequence
+     * includes the entire trace up to the first appearance of 'second'. If the
+     * trace has a 'first' before a 'second' then it returns null.
+     * 
+     * <pre>
+     * NOTE: x AP x cannot be true, so we will never have a counter-example
+     * in which 'first' == 'second'.
+     * </pre>
+     * 
+     * @param <T>
+     *            The node type of the trace
+     * @param trace
+     *            the trace we are operating on
+     * @return the sub-trace described above
+     */
     @Override
     public <T extends INode<T>> List<T> shorten(List<T> path) {
-        // TODO Auto-generated method stub
-        return null;
+        return AlwaysPrecedesInvariant.APShorten(path, first, second);
     }
 
     @Override
--- synoptic/src/synoptic/invariants/NFBiRelationInvariant.java
+++ synoptic/src/synoptic/invariants/NFBiRelationInvariant.java
@@ -6,17 +6,15 @@ import synoptic.model.event.EventType;
 import synoptic.model.interfaces.INode;
 
 /**
- * TODO: describe.
+ * Represents a birelational event invariant in the input 
+ * traces where the first event is never followed by the 
+ * second event.
  */
-public class NFBiRelationInvariant extends BinaryInvariant implements
-        IBiRelationalInvariant {
-
-    protected String orderingRelation;
+public class NFBiRelationInvariant extends BiRelationalInvariant {
 
     public NFBiRelationInvariant(EventType first, EventType second,
             String relation, String orderingRelation) {
-        super(first, second, relation);
-        this.orderingRelation = orderingRelation;
+        super(first, second, relation, orderingRelation);
     }
 
     @Override
@@ -24,10 +22,27 @@ public class NFBiRelationInvariant extends BinaryInvariant implements
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Returns a sub-trace of the input violating trace that looks like
+     * ...'first' ... 'second' where 'first' NFby 'second' is this invariant. It
+     * includes the section of the trace that precedes 'first' and ignores the
+     * section of the trace that follows 'second'. If the trace is not a
+     * counter-example trace (and therefore does not contain such a sequence)
+     * then it returns null.
+     * 
+     * <pre>
+     * NOTE: x NFby x is tricky
+     * </pre>
+     * 
+     * @param <T>
+     *            Type of the node in the trace
+     * @param trace
+     *            the trace we are operating on
+     * @return the sub-trace described above
+     */
     @Override
     public <T extends INode<T>> List<T> shorten(List<T> path) {
-        // TODO Auto-generated method stub
-        return null;
+        return NeverFollowedInvariant.NFShorten(path, first, second);
     }
 
     @Override
