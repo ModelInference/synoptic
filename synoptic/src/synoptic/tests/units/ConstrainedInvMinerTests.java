e534ab7ecc77dbd6b63add445943abed08302c68
Ivan Beschastnikh <ivan@cs.washington.edu>
2012-05-08 13:55:41 -0700

    Removed lowerBound and upperBound null checks.

--- synoptic/src/synoptic/tests/units/ConstrainedInvMinerTests.java
+++ synoptic/src/synoptic/tests/units/ConstrainedInvMinerTests.java
@@ -1,6 +1,6 @@
 package synoptic.tests.units;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -17,18 +17,19 @@ import synoptic.model.event.StringEventType;
 import synoptic.tests.SynopticTest;
 
 /**
- * Work in progress. Currently contains mostly copy-and-pasted code that will be useful.
+ * Work in progress. Currently contains mostly copy-and-pasted code that will be
+ * useful.
  */
 public class ConstrainedInvMinerTests extends SynopticTest {
-	ITOInvariantMiner miner;
-	
-	 @Override
-	 public void setUp() throws ParseException {
-		 super.setUp();
-		 miner = new ChainWalkingTOInvMiner();
-	 }
-	 
-  /**
+    ITOInvariantMiner miner;
+
+    @Override
+    public void setUp() throws ParseException {
+        super.setUp();
+        miner = new ChainWalkingTOInvMiner();
+    }
+
+    /**
      * Generates a TemporalInvariantSet based on a sequence of log events -- a
      * set of invariants that are mined from the log, and hold true for the
      * initial graph of the log.
@@ -42,13 +43,16 @@ public class ConstrainedInvMinerTests extends SynopticTest {
             boolean multipleRelations) throws Exception {
         ChainsTraceGraph inputGraph = genInitialLinearGraph(events);
         TemporalInvariantSet invs = miner.computeInvariants(inputGraph, false);
-        return ConstrainedInvMiner.computeInvariants(inputGraph, multipleRelations, invs);
+        return ConstrainedInvMiner.computeInvariants(inputGraph,
+                multipleRelations, invs);
     }
-	 
-   /**
-     * Compose a log in which "a AP b" is the only true invariant. Except that
-     * we making it the only invariant complicates the trace too much so we
-     * settle for also including the INITIAL AFby a invariant.
+
+    /**
+     * Compose a log in which "a AP b" is the only true invariant. But, instead
+     * of comparing the mined constrained invariants against the true
+     * constrained AP invariant, we compare against the standard AP invariant,
+     * using assertFalse. This checks that equality of constrained invariants
+     * works correctly.
      * 
      * @throws Exception
      */
@@ -60,13 +64,11 @@ public class ConstrainedInvMinerTests extends SynopticTest {
         TemporalInvariantSet trueInvs = new TemporalInvariantSet();
 
         trueInvs.add(new AlwaysFollowedInvariant(StringEventType
-                .newInitialStringEventType(), "a",
-                Event.defTimeRelationStr));
+                .newInitialStringEventType(), "a", Event.defTimeRelationStr));
         trueInvs.add(new AlwaysPrecedesInvariant("a", "b",
                 Event.defTimeRelationStr));
         logger.info("minedInvs: " + minedInvs.toString());
-        assertTrue(trueInvs.sameInvariants(minedInvs));
+        assertFalse(trueInvs.sameInvariants(minedInvs));
     }
- 
- 
+
 }
