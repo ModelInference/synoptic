e600d07b4c2e6444e5e367c97d75cb7aec66269a
Ivan Beschastnikh <ivan@cs.washington.edu>
2012-07-10 13:51:34 -0700

    Initial version of java lib to bridge with mcscm

---
+++ mcscm-bridge/tests/mcscm/McScMTests.java
@@ -0,0 +1,109 @@
+package mcscm;
+
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class McScMTests {
+
+    McScM bridge;
+    String verifyPath;
+    String scmFilePrefix;
+
+    protected static Logger logger;
+
+    @Before
+    public void setUp() {
+        // NOTE: We assume the tests are run from synoptic/mcscm-bridge/
+        verifyPath = "../mcscm-1.2/bin/verify.native";
+        scmFilePrefix = "./tests/mcscm/";
+
+        logger = Logger.getLogger("TestMcScM");
+        logger.setLevel(Level.INFO);
+        bridge = new McScM(verifyPath);
+    }
+
+    /**
+     * Reads a text file from the current directory and returns it as a single
+     * string.
+     * 
+     * @throws IOException
+     */
+    public String readScmFile(String filename) throws IOException {
+        String filePath = scmFilePrefix + filename;
+        BufferedReader in = new BufferedReader(new FileReader(filePath));
+
+        StringBuilder everything = new StringBuilder();
+        String line;
+        while ((line = in.readLine()) != null) {
+            everything.append(line);
+            everything.append("\n");
+        }
+        return everything.toString();
+    }
+
+    /**
+     * Empty scm input should result in a syntax error.
+     * 
+     * @throws IOException
+     */
+    @Test(expected = ScmSyntaxException.class)
+    public void testEmptyScmInput() throws IOException {
+        try {
+            bridge.verify("");
+        } catch (Exception e) {
+            logger.info("Verify threw an exception: " + e.toString());
+            fail("Verify should not fail.");
+        }
+
+        bridge.getVerifyResult();
+        fail("getVerifyResult should have thrown an exception.");
+    }
+
+    /**
+     * Test verify on a safe model.
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void testSafeScmInput() throws IOException {
+        String scmStr = readScmFile("ABP_safe.scm");
+
+        try {
+            bridge.verify(scmStr);
+        } catch (Exception e) {
+            logger.info("Verify threw an exception: " + e.toString());
+            fail("Verify should not fail.");
+        }
+
+        VerifyResult result = bridge.getVerifyResult();
+        assert (result.modelIsSafe());
+    }
+
+    /**
+     * Test verify on an unsafe model.
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void testUnsafeScmInput() throws IOException {
+        String scmStr = readScmFile("ABP_unsafe.scm");
+
+        try {
+            bridge.verify(scmStr);
+        } catch (Exception e) {
+            logger.info("Verify threw an exception: " + e.toString());
+            fail("Verify should not fail.");
+        }
+
+        VerifyResult result = bridge.getVerifyResult();
+        assert (!result.modelIsSafe());
+    }
+}
