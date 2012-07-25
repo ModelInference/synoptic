d6d527b4d4d41baca0ca193d430ec71a01364dcc
Ivan Beschastnikh <ivan@cs.washington.edu>
2012-07-25 13:12:13 -0700

    Added a simple test to dynoptic to make it pass.

---
+++ dynoptic/tests/dynoptic/model/alphabet/AlphabetTests.java
@@ -0,0 +1,11 @@
+package dynoptic.model.alphabet;
+
+import org.junit.Test;
+
+public class AlphabetTests {
+
+    @Test
+    public void createAlphabet() {
+        FSMAlphabet a = new FSMAlphabet();
+    }
+}
