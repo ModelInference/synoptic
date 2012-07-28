5d03ddcb26f61d9898f05eb721cfe4eb51f1caa6
Ivan Beschastnikh <ivan@cs.washington.edu>
2012-07-27 17:33:39 -0700

    More observed structures work

--- dynoptic/src/dynoptic/model/fifosys/gfsm/GFSMState.java
+++ dynoptic/src/dynoptic/model/fifosys/gfsm/GFSMState.java
@@ -1,11 +1,12 @@
 package dynoptic.model.fifosys.gfsm;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
 import dynoptic.model.alphabet.EventType;
 import dynoptic.model.fifosys.IMultiFSMState;
-import dynoptic.model.fifosys.cfsm.fsm.FSMState;
 import dynoptic.model.fifosys.gfsm.trace.ObservedFifoSysState;
 
 /**
@@ -30,40 +31,101 @@ public class GFSMState implements IMultiFSMState<GFSMState> {
 
     // CACHE optimization: whether or not any of the observed states were
     // terminal.
-    boolean isAccept;
+    // boolean isAccept;
 
     // CACHE optimization: the set of abstract transitions induced by the
     // concrete transitions. This is merely a cached version of the ground
     // truth.
-    Map<EventType, Set<FSMState>> transitions;
+    Map<EventType, Set<GFSMState>> transitions;
 
     public GFSMState() {
-        //
+        observedStates = new LinkedHashSet<ObservedFifoSysState>();
+        transitions = new LinkedHashMap<EventType, Set<GFSMState>>();
     }
 
     // //////////////////////////////////////////////////////////////////
 
     @Override
     public boolean isAccept() {
-        return isAccept;
+        for (ObservedFifoSysState s : observedStates) {
+            if (s.isAccept()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public Set<EventType> getTransitioningEvents() {
-        // TODO Auto-generated method stub
-        return null;
+        return transitions.keySet();
     }
 
     @Override
     public Set<GFSMState> getNextStates(EventType event) {
-        // TODO Auto-generated method stub
-        return null;
+        Set<GFSMState> ret = new LinkedHashSet<GFSMState>();
+        for (Set<GFSMState> following : transitions.values()) {
+            ret.addAll(following);
+        }
+        return ret;
     }
 
     @Override
     public boolean isAcceptForPid(int pid) {
-        // TODO Auto-generated method stub
+        for (ObservedFifoSysState s : observedStates) {
+            if (s.isAcceptForPid(pid)) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    // //////////////////////////////////////////////////////////////////
+
+    /** Adds a new observed state to this partition. */
+    public void add(ObservedFifoSysState s) {
+        assert !observedStates.contains(s);
+        observedStates.add(s);
+        cacheObservedParentTransitions(s);
+    }
+
+    /** Adds a new observed state to this partition. */
+    public void addAll(Set<ObservedFifoSysState> states) {
+        for (ObservedFifoSysState s : states) {
+            add(s);
+        }
+    }
+
+    /** Adds a new observed state to this partition. */
+    public void removeObservedState(ObservedFifoSysState s) {
+        assert observedStates.contains(s);
+        observedStates.remove(s);
+        recreateCachedTransitions();
+    }
+
+    /** Creates the transitions cache from scratch. */
+    private void recreateCachedTransitions() {
+        transitions.clear();
+
+        // Update the cached transitions for each observed state in this
+        // partition.
+        for (ObservedFifoSysState s : observedStates) {
+            cacheObservedParentTransitions(s);
+        }
+    }
+
+    /** Updates the cached transitions for a particular observed state. */
+    private void cacheObservedParentTransitions(ObservedFifoSysState s) {
+        for (EventType e : s.getTransitioningEvents()) {
+            GFSMState nextPartition = s.getNextState(e).getParent();
+            Set<GFSMState> partitions;
+            if (!transitions.containsKey(e)) {
+                partitions = new LinkedHashSet<GFSMState>();
+                transitions.put(e, partitions);
+            } else {
+                partitions = transitions.get(e);
+            }
+            partitions.add(nextPartition);
+        }
     }
 
 }
--- dynoptic/src/dynoptic/model/fifosys/gfsm/trace/ObservedEvent.java
+++ dynoptic/src/dynoptic/model/fifosys/gfsm/trace/ObservedEvent.java
@@ -2,6 +2,10 @@ package dynoptic.model.fifosys.gfsm.trace;
 
 import dynoptic.model.alphabet.EventType;
 
+/**
+ * Represents a state that was observed or mined from a log from an execution of
+ * a FIFO system.
+ */
 public class ObservedEvent {
     /** The type of the observed event. */
     final EventType eType;
