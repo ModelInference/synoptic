d944bedd149702b51a403bcb31ff4cb1bba4061e
Roykrong Sukkerd <rsukkerd@cs.washington.edu>
2012-10-25 19:26:49 -0700

    Fixed bug in EncodedAutomaton: in DFS, did not add transitions to visited nodes; Implemented hashCode() and equals() for FSMState so that DFS works; Fixed typo in FSM.DFS

--- dynoptic/src/dynoptic/model/fifosys/cfsm/fsm/FSMState.java
+++ dynoptic/src/dynoptic/model/fifosys/cfsm/fsm/FSMState.java
@@ -91,6 +91,52 @@ public class FSMState extends AbsFSMState<FSMState> {
         return "FSM_state: init[" + isInitial + "], accept[" + isAccept
                 + "] id[" + scmId + "]";
     }
+    
+    @Override
+    public int hashCode() {
+        int ret = 31;
+        ret = ret * 31 + (isAccept ? 1 : 0);
+        ret = ret * 31 + (isInitial ? 1 : 0);
+        ret = ret * 31 + transitions.hashCode();
+        ret = ret * 31 + pid;
+        ret = ret * 31 + scmId;
+        return 1;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        
+        if (!(other instanceof FSMState)) {
+            return false;
+        }
+        
+        FSMState state = (FSMState) other;
+        
+        if (state.isAccept != this.isAccept) {
+            return false;
+        }
+        
+        if (state.isInitial != this.isInitial) {
+            return false;
+        }
+        
+        if (!state.transitions.equals(this.transitions)) {
+            return false;
+        }
+        
+        if (state.pid != this.pid) {
+            return false;
+        }
+        
+        if (state.scmId != this.scmId) {
+            return false;
+        }
+        
+        return true;
+    }
 
     // //////////////////////////////////////////////////////////////////
 
