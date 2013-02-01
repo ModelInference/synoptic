9390c6815029b58f9bb0ffeb73aa3efabad485ef
Roykrong Sukkerd <rsukkerd@cs.washington.edu>
2013-01-31 18:42:38 -0800

    Moved all Daikonizer's files to daikonizer package so that Synoptic can import it, added reference to Daikonizer to Synoptic

--- daikonizer/src/daikonizer/DaikonTrace.java
+++ daikonizer/src/daikonizer/DaikonTrace.java
@@ -1,3 +1,5 @@
+package daikonizer;
+
 import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
--- daikonizer/src/daikonizer/DirectDaikonizer.java
+++ daikonizer/src/daikonizer/DirectDaikonizer.java
@@ -1,3 +1,5 @@
+package daikonizer;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
