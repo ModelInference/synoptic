2a2b5990fdee71a911dd842b446711ab356c1820
Roykrong Sukkerd <rsukkerd@cs.washington.edu>
2013-02-07 19:16:36 -0800

    Implemented a method to determine type of a value

--- synoptic/src/synoptic/model/state/State.java
+++ synoptic/src/synoptic/model/state/State.java
@@ -16,7 +16,10 @@ import synoptic.main.parser.ParseException;
 /**
  * Represents a state parsed from a log file. A state is a set of
  * variable-value pairs, defined at a point in time (in an execution trace).
- * A String representation of a state is in the form var=value,...,var=value.
+ * 
+ * A String representation of a state (from the log) is in the form
+ * id=value,...,id=value. It has no type information. Therefore, State class
+ * must determine the types of the variables.
  * 
  * @author rsukkerd
  *
@@ -28,6 +31,18 @@ public class State implements Iterable<Map.Entry<DaikonVar, String>> {
     private static final Pattern matchAssign = Pattern
             .compile("(?<ID>[^\\s=]+)\\s*=\\s*(?<VALUE>[^\\s=]+)");
     
+    /** Type patterns -- boolean, int, hashcode, double and String **/
+    private static final Pattern matchBoolean = Pattern
+            .compile("true|false");
+    private static final Pattern matchInt = Pattern
+            .compile("[+-]?[1-9][0-9]*");
+    private static final Pattern matchHashcode = Pattern
+            .compile("0x([0-9]|[a-fA-F])+");
+    private static final Pattern matchDouble = Pattern
+            .compile("[+-]?(0|[1-9][0-9]*)(.[0-9]+)?([eE][+-]?[0-9]+)?");
+    private static final Pattern matchString = Pattern
+            .compile("\"[^\"]*\"");
+
     /** A String representation of this state. */
     private final String stateString;
     /** A map from variables to values. */
@@ -39,6 +54,11 @@ public class State implements Iterable<Map.Entry<DaikonVar, String>> {
         buildStateMap();
     }
     
+    /**
+     * Builds a map from variables to values.
+     * 
+     * @throws ParseException
+     */
     private void buildStateMap() throws ParseException {
         String[] pairs = stateString.split(DELIM);
         
@@ -60,9 +80,33 @@ public class State implements Iterable<Map.Entry<DaikonVar, String>> {
         }
     }
     
-    private DaikonType determineType(String value) {
-        // TODO: figure out type of value
-        return null;
+    /**
+     * Determines the type of this value.
+     * 
+     * @throws ParseException
+     */
+    private DaikonType determineType(String value) throws ParseException {
+        Matcher matcher = matchBoolean.matcher(value);
+        if (matcher.matches()) {
+            return DaikonType.BOOLEAN;
+        }
+        matcher = matchInt.matcher(value);
+        if (matcher.matches()) {
+            return DaikonType.INT;
+        }
+        matcher = matchHashcode.matcher(value);
+        if (matcher.matches()) {
+            return DaikonType.HASHCODE;
+        }
+        matcher = matchDouble.matcher(value);
+        if (matcher.matches()) {
+            return DaikonType.DOUBLE;
+        }
+        matcher = matchString.matcher(value);
+        if (matcher.matches()) {
+            return DaikonType.STRING;
+        }
+        throw new ParseException(value + " is not a recognizable type");
     }
     
     public Set<DaikonVar> getVariables() {
