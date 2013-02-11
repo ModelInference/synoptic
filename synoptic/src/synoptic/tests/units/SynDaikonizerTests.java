2b9930b5f908ab51a1c9cfc5690a449f2f83b865
Roykrong Sukkerd <rsukkerd@cs.washington.edu>
2013-02-10 19:24:46 -0800

    Added tests for SynDaikonizer

---
+++ synoptic/src/synoptic/tests/units/SynDaikonizerTests.java
@@ -0,0 +1,72 @@
+package synoptic.tests.units;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import daikon.inv.Invariant;
+
+import synoptic.model.state.State;
+import synoptic.model.state.SynDaikonizer;
+import synoptic.tests.SynopticTest;
+
+public class SynDaikonizerTests extends SynopticTest {
+    
+    /**
+     * All states have 1 variable: x.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void oneVariableTest() throws Exception {
+        State s1 = new State("x=-1");
+        State s2 = new State("x=0");
+        State s3 = new State("x=1");
+        detectDaikonInvariants(s1, s2, s3);
+    }
+    
+    /**
+     * All states have 2 variables: x and y.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void twoVariablesTest() throws Exception {
+        State s1 = new State("x=1,y=1");
+        State s2 = new State("x=2,y=2");
+        State s3 = new State("x=3,y=3");
+        detectDaikonInvariants(s1, s2, s3);
+    }
+    
+    /**
+     * All states have 3 variables: x, y and z.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void threeVariablesTest() throws Exception {
+        State s1 = new State("x=1,y=1,z=2");
+        State s2 = new State("x=2,y=-1,z=1");
+        State s3 = new State("x=3,y=0,z=3");
+        detectDaikonInvariants(s1, s2, s3);
+    }
+    
+    /**
+     * Detects invariants of the states and prints them.
+     */
+    private void detectDaikonInvariants(State... states) throws Exception {
+        SynDaikonizer daikonizer = new SynDaikonizer();
+        for (State state : states) {
+            daikonizer.addInstance(state);
+        }
+        List<Invariant> invariants = daikonizer.getDaikonInvariants();
+        System.out.println("Invariants of the states:");
+        for (State state : states) {
+            System.out.println("\t" + state);
+        }
+        System.out.println("are:");
+        for (Invariant inv : invariants) {
+            System.out.println("\t" + inv);
+        }
+    }
+}
