986a1960f9d663d87252927596d80d9765fc95ed
Jenny Abrahamson <jabrah@cs.washington.edu>
2013-03-06 12:15:46 -0800

    Packaging ShiVector src

---
+++ shivector/src/aspects/ShivSocketOutputStream.java
@@ -0,0 +1,48 @@
+package aspects;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * OutputStream to prepend VectorClocks on outgoing messages.
+ * 
+ * @author jennyabrahamson
+ */
+public class ShivSocketOutputStream extends OutputStream {
+
+    private OutputStream out;
+    private VectorClock clock;
+
+    public ShivSocketOutputStream(OutputStream out, VectorClock clock) {
+        this.out = out;
+        this.clock = clock;
+    }
+
+    @Override
+    public void close() throws IOException {
+        out.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        out.flush();
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        clock.writeVectorClock(out, b.length);
+        out.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        clock.writeVectorClock(out, len);
+        out.write(b, off, len);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        clock.writeVectorClock(out, 1);
+        out.write(b);
+    }
+}
