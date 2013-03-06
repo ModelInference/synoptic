b91377a7d701fc4815f318a1ca6933ea20311f43
Jenny Abrahamson <jabrah@cs.washington.edu>
2013-03-06 13:37:27 -0800

    Sample app build file

--- shivector/shivector.sh
+++
@@ -1,3 +0,0 @@
-SYNOPTIC_LIB=/Users/jennyabrahamson/Documents/research/synoptic/lib/
-
-ajc -cp ${SYNOPTIC_LIB}aspectjrt.jar:${SYNOPTIC_LIB}junit-4.9b2.jar -inpath ${SYNOPTIC_LIB}shivector.jar -1.5 -sourceroots "src"
