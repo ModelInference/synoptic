6163d914ffc08fc5253224d0f7419b47d1c201c0
Roykrong Sukkerd <rsukkerd@cs.washington.edu>
2013-03-24 21:49:06 -0700

    Added softlink to turnstile-example to traces/EndToEndTests

--- synoptic/src/synoptic/tests/integration/EndToEndMainTests.java
+++ synoptic/src/synoptic/tests/integration/EndToEndMainTests.java
@@ -46,10 +46,9 @@ public class EndToEndMainTests extends SynopticTest {
         Collection<Object[]> argsList = new LinkedList<Object[]>();
 
         // List of input sub-dirs that contains end-to-end test examples.
-//        String[] testPaths = { "mid_branching", "osx-login-example",
-//                "shopping-cart-example", "ticket-reservation-example",
-//                "turnstile-example" };
-        String[] testPaths = { "turnstile-example" };
+        String[] testPaths = { "mid_branching", "osx-login-example",
+                "shopping-cart-example", "ticket-reservation-example",
+                "turnstile-example" };
 
         // Determine where the input traces/args are located -- try two options:
         // tracesPath1 and tracesPath2. Test using testPaths[0].
