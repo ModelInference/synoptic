4c9658e8afe2ce31eb8906bd429d979ad6e74331
Ivan Beschastnikh <ivan@cs.washington.edu>
2013-08-20 00:11:59 -0700

    invarimint refactoring and fixes

--- InvariMint/src/algorithms/InvariMintKTails.java
+++ InvariMint/src/algorithms/InvariMintKTails.java
@@ -1,15 +1,24 @@
 package algorithms;
 
 import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import main.InvariMintOptions;
+import model.EventTypeEncodings;
+import model.InvModel;
 import model.InvsModel;
 
 import synoptic.algorithms.KTails;
+import synoptic.invariants.TOInitialInvariant;
 import synoptic.invariants.miners.ITOInvariantMiner;
 import synoptic.invariants.miners.KTailInvariantMiner;
+import synoptic.model.EventNode;
 import synoptic.model.PartitionGraph;
+import synoptic.model.event.Event;
+import synoptic.model.event.EventType;
 import synoptic.model.export.GraphExporter;
+import synoptic.model.interfaces.ITransition;
 
 public class InvariMintKTails extends PGraphInvariMint {
 
@@ -31,12 +40,34 @@ public class InvariMintKTails extends PGraphInvariMint {
 
         ITOInvariantMiner ktailsMiner = new KTailInvariantMiner(
                 opts.kTailLength);
-        InvsModel model = super.runInvariMint(ktailsMiner);
+
+        logger.info("Initializing with an all-accepting model.");
+        this.initializeModel();
+
+        logger.info("\n\nApplying 'Traces start with Init' inv.");
+
+        // Add the "^Initial[^Terminal]*Terminal$" invariant.
+        InvModel initialTerminalInv = new InvModel(new TOInitialInvariant(
+                initialEvent, terminalEvent, Event.defTimeRelationStr),
+                encodings);
+        invMintModel.intersectWith(initialTerminalInv);
+
+        logger.info("Mining Invs.");
+
+        // Mine invariants using the specialized invMiner.
+        minedInvs = this.mineInvariants(ktailsMiner);
+
+        logger.info("Intersecting current model with mined invs with minimize intersections="
+                + opts.minimizeIntersections);
+
+        // Intersect current model with mined invariants.
+        invMintModel = InvComposition.intersectModelWithInvs(minedInvs,
+                opts.minimizeIntersections, invMintModel);
 
         logger.info("InvariMint mined properties: "
                 + minedInvs.toPrettyString());
 
-        return model;
+        return invMintModel;
 
     }
 
@@ -60,4 +91,56 @@ public class InvariMintKTails extends PGraphInvariMint {
         stdAlgPGraph = KTails.performKTails(traceGraph, opts.kTailLength);
     }
 
+    // //////////////////////////////////////////////////////////////////////
+
+    /**
+     * Initialize an all-accepting InvariMint DFA.
+     * 
+     * @throws IOException
+     */
+    private void initializeModel() throws IOException {
+        assert invMintModel == null;
+
+        logger.fine("Creating EventType encoding.");
+        encodings = new EventTypeEncodings(getAllEventTypes());
+
+        // Initial model will accept all Strings.
+        logger.fine("Creating an initial, all-accepting, model.");
+        invMintModel = new InvsModel(encodings);
+    }
+
+    /**
+     * Returns all event types present in the traceGraph (including INITIAL and
+     * TERMINAL).
+     */
+    private Set<EventType> getAllEventTypes() {
+        EventNode initNode = traceGraph.getDummyInitialNode();
+        Set<EventType> eTypes = new LinkedHashSet<EventType>();
+
+        // Iterate through all the traces -- each transition from the
+        // INITIAL node connects\holds a single trace.
+        for (ITransition<EventNode> initTrans : initNode.getAllTransitions()) {
+
+            EventNode cur = initTrans.getTarget();
+            EventType first = initNode.getEType();
+            EventType second = cur.getEType();
+
+            while (true) {
+                if (!eTypes.contains(first)) {
+                    eTypes.add(first);
+                }
+                if (cur.getAllTransitions().size() == 0) {
+                    // Add terminal event to the set.
+                    if (!eTypes.contains(second)) {
+                        eTypes.add(second);
+                    }
+                    break;
+                }
+                cur = cur.getAllTransitions().get(0).getTarget();
+                first = second;
+                second = cur.getEType();
+            }
+        }
+        return eTypes;
+    }
 }
--- InvariMint/src/algorithms/InvariMintSynoptic.java
+++ InvariMint/src/algorithms/InvariMintSynoptic.java
@@ -1,12 +1,23 @@
 package algorithms;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
 import main.InvariMintOptions;
+import model.EventTypeEncodings;
+import model.InvModel;
 import model.InvsModel;
 
 import synoptic.algorithms.Bisimulation;
+import synoptic.invariants.TOInitialTerminalInvariant;
+import synoptic.invariants.TemporalInvariantSet;
 import synoptic.invariants.miners.ChainWalkingTOInvMiner;
 import synoptic.invariants.miners.ITOInvariantMiner;
+import synoptic.invariants.miners.ImmediateInvariantMiner;
 import synoptic.model.PartitionGraph;
+import synoptic.model.event.Event;
+import synoptic.model.event.EventType;
 
 public class InvariMintSynoptic extends PGraphInvariMint {
 
@@ -27,7 +38,36 @@ public class InvariMintSynoptic extends PGraphInvariMint {
         assert opts.invMintSynoptic;
 
         ITOInvariantMiner synMiner = new ChainWalkingTOInvMiner();
-        return super.runInvariMint(synMiner);
+
+        logger.info("Initializing with NIFby invs.");
+
+        // NIFby invariants.
+        this.initializeModel();
+
+        logger.info("\n\nApplying Init AFby Term inv.");
+
+        // TODO: It is unclear that InvariMint-Synoptic needs such a strong
+        // Init/Term invariant.
+
+        // Add the "^Initial[^(Initial|Terminal)]*Terminal$" invariant
+        InvModel initialTerminalInv = new InvModel(
+                new TOInitialTerminalInvariant(initialEvent, terminalEvent,
+                        Event.defTimeRelationStr), encodings);
+        invMintModel.intersectWith(initialTerminalInv);
+
+        logger.info("Mining Invs.");
+
+        // Mine invariants using the specialized invMiner.
+        minedInvs = this.mineInvariants(synMiner);
+
+        logger.info("Intersecting current model with mined invs with minimize intersections="
+                + opts.minimizeIntersections);
+
+        // Intersect current model with mined invariants.
+        invMintModel = InvComposition.intersectModelWithInvs(minedInvs,
+                opts.minimizeIntersections, invMintModel);
+
+        return invMintModel;
     }
 
     @Override
@@ -42,6 +82,36 @@ public class InvariMintSynoptic extends PGraphInvariMint {
         Bisimulation.splitUntilAllInvsSatisfied(synModel);
         Bisimulation.mergePartitions(synModel);
         stdAlgPGraph = synModel;
+    }
+
+    // ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Initialize the InvariMint DFA from mined NIFby invariants.
+     * 
+     * @throws IOException
+     */
+    private void initializeModel() throws IOException {
+        assert invMintModel == null;
+
+        logger.fine("Mining NIFby invariant(s).");
+        ImmediateInvariantMiner miner = new ImmediateInvariantMiner(traceGraph);
+        TemporalInvariantSet NIFbys = miner.getNIFbyInvariants();
+        logger.fine("Mined " + NIFbys.numInvariants() + " NIFby invariant(s).");
+        // logger.fine(NIFbys.toPrettyString());
+
+        logger.fine("Creating EvenType encoding.");
+        Set<EventType> allEvents = new HashSet<EventType>(miner.getEventTypes());
+        encodings = new EventTypeEncodings(allEvents);
+
+        // Initial model will accept all Strings.
+        logger.fine("Creating an initial, all-accepting, model.");
+        invMintModel = new InvsModel(encodings);
+
+        logger.fine("Intersecting model with mined NIFby invariants (minimizeIntersections="
+                + opts.minimizeIntersections + ")");
+        invMintModel = InvComposition.intersectModelWithInvs(NIFbys,
+                opts.minimizeIntersections, invMintModel);
     }
 
 }
