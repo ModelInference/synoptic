ce6ce12fcd856fb7496930a620c453ca3be74d69
Tony Ohmann <ohmann@cs.umass.edu>
2013-10-31 11:27:40 -0400

    Outlined structure of JSON exporter class and implemented the 'log' part of the output

--- synoptic.sh
+++ synoptic.sh
@@ -3,4 +3,4 @@
 # Runs Synoptic from the compiled class files, passing all command
 # line argument directly to main().
 
-java -ea -cp ./lib/junit-4.9b2.jar:./lib/plume.jar:./lib/daikonizer.jar:./synoptic/bin/:./daikonizer/bin/ synoptic.main.SynopticMain $*
+java -ea -cp ./lib/junit-4.9b2.jar:./lib/plume.jar:./lib/daikonizer.jar:./lib/json-simple-1.1.1.jar:./synoptic/bin/:./daikonizer/bin/ synoptic.main.SynopticMain $*
--- synoptic/src/synoptic/model/export/JsonExporter.java
+++ synoptic/src/synoptic/model/export/JsonExporter.java
@@ -1,10 +1,27 @@
 package synoptic.model.export;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.json.simple.JSONValue;
+
+import synoptic.model.EventNode;
+import synoptic.model.Partition;
+import synoptic.model.PartitionGraph;
+import synoptic.model.event.EventType;
 import synoptic.model.interfaces.IGraph;
 import synoptic.model.interfaces.INode;
 
 /**
- * Outputs a partition graph as a JSON object.
+ * Outputs a partition graph as a JSON object. Uses the JSON-simple library,
+ * licensed under Apache 2.0 (the same license as Synoptic and its
+ * sub-projects), available at https://code.google.com/p/json-simple/.
  * 
  * @author Tony Ohmann (ohmann@cs.umass.edu)
  * @param <T>
@@ -19,11 +36,92 @@ public class JsonExporter {
      * @param baseFilename
      *            The filename to which the JSON object should be written sans
      *            file extension
-     * @param pGraph
+     * @param graph
      *            The partition graph to output
      */
     public static <T extends INode<T>> void exportJsonObject(
-            String baseFilename, IGraph<T> pGraph) {
-        //
+            String baseFilename, IGraph<T> graph) {
+
+        // The graph must be a partition graph
+        assert graph instanceof PartitionGraph;
+        PartitionGraph pGraph = (PartitionGraph) graph;
+
+        Map<String, Object> finalModelMap = new LinkedHashMap<String, Object>();
+
+        // Add log to final model map
+        List<Map<String, Object>> logListOfTraces = makeLogJSON(pGraph);
+        finalModelMap.put("log", logListOfTraces);
+
+        // Add partitions to final model map
+
+        // Add invariants to final model map
+
+        // Output the final model map as a JSON object
+        try {
+            PrintWriter output = new PrintWriter(baseFilename + ".json");
+            JSONValue.writeJSONString(finalModelMap, output);
+            output.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Creates a JSON sub-object for the log containing a list of traces
+     * 
+     * @param pGraph
+     *            The partition graph whose log we're outputting
+     */
+    private static List<Map<String, Object>> makeLogJSON(PartitionGraph pGraph) {
+        // The log (list of traces) to go into the JSON object
+        List<Map<String, Object>> logListOfTraces = new LinkedList<Map<String, Object>>();
+
+        Set<Partition> allPart = pGraph.getNodes();
+        Partition initialPart = null;
+        for (Partition part : allPart) {
+            if (part.isInitial()) {
+                initialPart = part;
+                break;
+            }
+        }
+
+        int traceNum = 0;
+        for (EventNode startingEvent : initialPart.getEventNodes().iterator()
+                .next().getAllSuccessors()) {
+            // One trace, contains the trace number and a list of events
+            Map<String, Object> singleTraceMap = new LinkedHashMap<String, Object>();
+            // List of events
+            List<Map<String, Object>> singleTraceEventsList = new LinkedList<Map<String, Object>>();
+
+            for (EventNode event = startingEvent; !event.isTerminal(); event = event
+                    .getAllSuccessors().iterator().next()) {
+                // One event, contains the event type and the timestamp
+                Map<String, Object> singleEventMap = new LinkedHashMap<String, Object>();
+
+                EventType evType = event.getEType();
+
+                // Populate this event's type
+                singleEventMap.put("eventType", evType.toString());
+
+                // Populate this event's time if it's not INITIAL or TERMINAL
+                if (!evType.isSpecialEventType()) {
+                    singleEventMap.put("timestamp", event.getTime());
+                }
+
+                singleTraceEventsList.add(singleEventMap);
+            }
+
+            // Populate the single trace
+            singleTraceMap.put("traceID", traceNum++);
+            singleTraceMap.put("events", singleTraceEventsList);
+
+            // Put the trace into the log's list of traces
+            logListOfTraces.add(singleTraceMap);
+        }
+
+        return logListOfTraces;
     }
 }
