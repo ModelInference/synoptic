ec8f9fc67ec132b21ff6a02eae14bf8b1652e4a0
Michael Herzberg <michaelherzberg91@gmail.com>
2013-11-25 16:17:21 -0500

    Added unit test for node time stamp normalization.

---
+++ synoptic/src/synoptic/tests/units/NodeNormalizationTests.java
@@ -0,0 +1,56 @@
+package synoptic.tests.units;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import synoptic.main.SynopticMain;
+import synoptic.main.parser.ParseException;
+import synoptic.model.EventNode;
+import synoptic.tests.PynopticTest;
+import synoptic.util.InternalSynopticException;
+import synoptic.util.time.DTotalTime;
+import synoptic.util.time.ITime;
+
+public class NodeNormalizationTests extends PynopticTest {
+
+    private String[] events = { "a 2", "b 3", "c 5", "d 6", "--", "a 10",
+            "b 11", "c 14", "d 16" };
+
+    /**
+     * Verifies that, after node normalization, there is one node with time
+     * stamp 0 and one with time stamp 1, and that all other time stamps are in
+     * between
+     * 
+     * @throws InternalSynopticException
+     * @throws ParseException
+     */
+    @Test
+    public void nodeNormalizationTest() throws InternalSynopticException,
+            ParseException {
+        ArrayList<EventNode> nodes = parseLogEvents(events, genITimeParser());
+
+        SynopticMain.normalizeEventNodes(nodes);
+
+        boolean zeroFound = false;
+        ITime zeroTime = new DTotalTime(0);
+        boolean oneFound = false;
+        ITime oneTime = new DTotalTime(1);
+
+        for (EventNode node : nodes) {
+            assertEquals(true, zeroTime.compareTo(node.getTime()) <= 0);
+            assertEquals(true, oneTime.compareTo(node.getTime()) >= 0);
+
+            if (zeroTime.compareTo(node.getTime()) == 0) {
+                zeroFound = true;
+            } else if (oneTime.compareTo(node.getTime()) == 0) {
+                oneFound = true;
+            }
+        }
+
+        assert (zeroFound);
+        assert (oneFound);
+    }
+}
