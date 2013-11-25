c8c3a7b0e41de6e5b7000166b1062a434e0e16da
Michael Herzberg <michaelherzberg91@gmail.com>
2013-11-24 22:40:01 -0500

    Added Interupter invariant fsmcheck classes.

---
+++ synoptic/src/synoptic/invariants/fsmcheck/IntrInvFsms.java
@@ -0,0 +1,68 @@
+package synoptic.invariants.fsmcheck;
+
+import java.util.BitSet;
+import java.util.List;
+
+import synoptic.invariants.BinaryInvariant;
+import synoptic.model.interfaces.INode;
+
+public class IntrInvFsms<T extends INode<T>> extends FsmStateSet<T> {
+    /**
+     * <pre>
+     * State 1: Accept state (no A seen, maybe some Bs seen)
+     * State 2: Accept state (one A seen)
+     * State 3: Permanent failed state (two As seen and no B in between)
+     * 
+     * </pre>
+     */
+
+    public IntrInvFsms(List<BinaryInvariant> invs) {
+        super(invs, 3);
+    }
+
+    @Override
+    public boolean isFail() {
+        return !sets.get(2).isEmpty();
+    }
+
+    @Override
+    public BitSet whichFail() {
+        return (BitSet) sets.get(2).clone();
+    }
+
+    @Override
+    public BitSet whichPermanentFail() {
+        return (BitSet) sets.get(2).clone();
+    }
+
+    @Override
+    public void setInitial(T input) {
+        BitSet isA = getInputCopy(0, input);
+        sets.set(1, (BitSet) isA.clone());
+        isA.flip(0, count);
+        sets.set(0, isA);
+    }
+
+    @Override
+    public void transition(T input) {
+        /*
+         * s1 = (s1 | s2) & isB s2 = s1 & isA s3 = s3 | (s2 & isA)
+         */
+
+        BitSet isA = getInputInvariantsDependencies(0, input);
+        BitSet isB = getInputInvariantsDependencies(1, input);
+        BitSet s1 = sets.get(0);
+        BitSet s2 = sets.get(1);
+        BitSet s3 = sets.get(2);
+
+        s1.or(s2);
+        s1.and(isB);
+
+        s2 = s1;
+        s2.and(isA);
+
+        BitSet t = (BitSet) s2.clone();
+        t.and(isA);
+        s3.or(t);
+    }
+}
---
+++ synoptic/src/synoptic/invariants/fsmcheck/IntrTracingSet.java
@@ -0,0 +1,107 @@
+package synoptic.invariants.fsmcheck;
+
+import synoptic.invariants.BinaryInvariant;
+import synoptic.model.event.EventType;
+import synoptic.model.interfaces.INode;
+
+public class IntrTracingSet<T extends INode<T>> extends TracingStateSet<T> {
+    HistoryNode<T> aNotSeen;
+    HistoryNode<T> aSeenOnce;
+    HistoryNode<T> aSeenMoreThanOnce;
+
+    EventType a, b;
+
+    public IntrTracingSet(EventType a, EventType b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public IntrTracingSet(BinaryInvariant inv) {
+        this(inv.getFirst(), inv.getSecond());
+    }
+
+    @Override
+    public void setInitial(T x) {
+        EventType name = x.getEType();
+        HistoryNode<T> newHistory = new HistoryNode<T>(x, null, 1);
+        aNotSeen = aSeenOnce = aSeenMoreThanOnce = null;
+        if (a.equals(name)) {
+            aSeenOnce = newHistory;
+        } else {
+            aNotSeen = newHistory;
+        }
+    }
+
+    @Override
+    public void transition(T x) {
+        EventType name = x.getEType();
+
+        if (b.equals(name)) {
+            aNotSeen = preferShorter(aSeenOnce, aNotSeen);
+        } else if (a.equals(name)) {
+            aSeenOnce = aNotSeen;
+            aSeenMoreThanOnce = null;
+        }
+
+        // Advance history for all states.
+        aNotSeen = extend(x, aNotSeen);
+        aSeenOnce = extend(x, aSeenOnce);
+        aSeenMoreThanOnce = extend(x, aSeenMoreThanOnce);
+    }
+
+    @Override
+    public HistoryNode<T> failpath() {
+        return aSeenMoreThanOnce;
+    }
+
+    @Override
+    public IntrTracingSet<T> copy() {
+        IntrTracingSet<T> result = new IntrTracingSet<T>(a, b);
+        result.aNotSeen = aNotSeen;
+        result.aSeenOnce = aSeenOnce;
+        result.aSeenMoreThanOnce = aSeenMoreThanOnce;
+        return result;
+    }
+
+    @Override
+    public void mergeWith(TracingStateSet<T> other) {
+        IntrTracingSet<T> casted = (IntrTracingSet<T>) other;
+        aNotSeen = preferShorter(aNotSeen, casted.aNotSeen);
+        aSeenOnce = preferShorter(aSeenOnce, casted.aSeenOnce);
+        aSeenMoreThanOnce = preferShorter(aSeenMoreThanOnce,
+                casted.aSeenMoreThanOnce);
+    }
+
+    @Override
+    public boolean isSubset(TracingStateSet<T> other) {
+        IntrTracingSet<T> casted = (IntrTracingSet<T>) other;
+        if (casted.aNotSeen == null) {
+            if (aNotSeen != null) {
+                return false;
+            }
+        }
+        if (casted.aSeenOnce == null) {
+            if (aSeenOnce != null) {
+                return false;
+            }
+        }
+        if (casted.aSeenMoreThanOnce == null) {
+            if (aSeenMoreThanOnce != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("Intr: ");
+        appendWNull(result, aSeenMoreThanOnce); // Failure case first.
+        result.append(" | ");
+        appendWNull(result, aSeenOnce);
+        result.append(" | ");
+        appendWNull(result, aNotSeen);
+        return result.toString();
+    }
+}
