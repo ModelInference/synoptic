b14dbd48f0613168deb5789f6f58e57bc0f5ab84
Tony Ohmann <ohmann@cs.umass.edu>
2013-12-09 17:42:31 -0500

    Changed IntrByLower to use same state ordering as IntrByUpper

--- synoptic/src/synoptic/invariants/fsmcheck/IntrByLowerTracingSet.java
+++ synoptic/src/synoptic/invariants/fsmcheck/IntrByLowerTracingSet.java
@@ -15,8 +15,9 @@ import synoptic.util.time.ITime;
  * State0 = states.get(0): A not seen <br />
  * State1 = states.get(1): First A seen, or second A seen after B within time
  * bound (potentially a new "first A") <br />
- * State2 = states.get(2): B seen after A <br />
- * State3 = states.get(3): Two A's seen without B, or A then B then A seen out
+ * State2 = states.get(2): First A seen, then something other than A or B seen <br />
+ * State3 = states.get(3): B seen after A <br />
+ * State4 = states.get(4): Two A's seen without B, or A then B then A seen out
  * of time bound (permanent reject state)
  * 
  * @param <T>
@@ -51,49 +52,49 @@ public class IntrByLowerTracingSet<T extends INode<T>> extends
             states.set(1, statesOld.get(0));
         }
 
-        // State1 -> State2
-        if (statesOld.get(1) != null && isB) {
-            states.set(2, statesOld.get(1));
-        }
-
-        // State2 -> State2
-        if (statesOld.get(2) != null && !isA) {
-            states.set(2, preferMinTime(statesOld.get(2), states.get(2)));
-        }
-
-        // State4 -> State2
-        if (statesOld.get(4) != null && isB) {
-            states.set(2, preferMinTime(statesOld.get(4), states.get(2)));
-        }
-
         // State1 -> State3
-        if (statesOld.get(1) != null && isA) {
+        if (statesOld.get(1) != null && isB) {
             states.set(3, statesOld.get(1));
         }
 
-        // State2 -> State3
-        if (statesOld.get(2) != null && isA && outOfBound.get(2)) {
-            states.set(3, preferMinTime(statesOld.get(2), states.get(3)));
-        }
-
         // State3 -> State3
-        if (statesOld.get(3) != null) {
+        if (statesOld.get(3) != null && !isA) {
             states.set(3, preferMinTime(statesOld.get(3), states.get(3)));
         }
 
-        // State1 -> State4
-        if (statesOld.get(1) != null && !isA && !isB) {
-            states.set(4, preferMinTime(statesOld.get(1), states.get(4)));
+        // State2 -> State3
+        if (statesOld.get(2) != null && isB) {
+            states.set(3, preferMinTime(statesOld.get(2), states.get(3)));
         }
 
-        // State2 -> State4
-        if (statesOld.get(2) != null && isA && !outOfBound.get(2)) {
-            states.set(4, preferMinTime(statesOld.get(2), states.get(4)));
+        // State1 -> State4
+        if (statesOld.get(1) != null && isA) {
+            states.set(4, statesOld.get(1));
+        }
+
+        // State3 -> State4
+        if (statesOld.get(3) != null && isA && outOfBound.get(3)) {
+            states.set(4, preferMinTime(statesOld.get(3), states.get(4)));
         }
 
         // State4 -> State4
-        if (statesOld.get(4) != null && !isA && !isB) {
+        if (statesOld.get(4) != null) {
             states.set(4, preferMinTime(statesOld.get(4), states.get(4)));
+        }
+
+        // State1 -> State2
+        if (statesOld.get(1) != null && !isA && !isB) {
+            states.set(2, preferMinTime(statesOld.get(1), states.get(2)));
+        }
+
+        // State3 -> State2
+        if (statesOld.get(3) != null && isA && !outOfBound.get(3)) {
+            states.set(2, preferMinTime(statesOld.get(3), states.get(2)));
+        }
+
+        // State2 -> State2
+        if (statesOld.get(2) != null && !isA && !isB) {
+            states.set(2, preferMinTime(statesOld.get(2), states.get(2)));
         }
 
         // Retrieve the previously-found min time delta
@@ -104,15 +105,15 @@ public class IntrByLowerTracingSet<T extends INode<T>> extends
 
         // Update the running time deltas of any states which require it. State0
         // disregards time. State1 sets time to 0, which is the default value.
-        // State2,3,4 require updates.
-        if (states.get(2) != null) {
-            tRunning.set(2, tMin.incrBy(states.get(2).tDelta));
-        }
+        // State3,3,4 require updates.
         if (states.get(3) != null) {
             tRunning.set(3, tMin.incrBy(states.get(3).tDelta));
         }
         if (states.get(4) != null) {
             tRunning.set(4, tMin.incrBy(states.get(4).tDelta));
+        }
+        if (states.get(2) != null) {
+            tRunning.set(2, tMin.incrBy(states.get(2).tDelta));
         }
 
         // Extend histories for each state
@@ -121,23 +122,23 @@ public class IntrByLowerTracingSet<T extends INode<T>> extends
                     extend(input, states.get(i), transitions, tRunning.get(i)));
         }
 
-        // The violation subpath started whenever we reach State1 or State4
+        // The violation subpath started whenever we reach State1 or State2
         if (states.get(1) != null) {
             states.get(1).startViolationHere();
         }
-        if (states.get(4) != null) {
-            states.get(4).startViolationHere();
+        if (states.get(2) != null) {
+            states.get(2).startViolationHere();
         }
 
-        // The violation subpath ended if we just reached State3
-        if (states.get(3) != null && statesOld.get(3) == null) {
-            states.get(3).endViolationHere();
+        // The violation subpath ended if we just reached State4
+        if (states.get(4) != null && statesOld.get(4) == null) {
+            states.get(4).endViolationHere();
         }
     }
 
     @Override
     public HistoryNode<T> failpath() {
-        return states.get(3);
+        return states.get(4);
     }
 
     @Override
