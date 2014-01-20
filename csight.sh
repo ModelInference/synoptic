b93eef3249c4b8a6ff75df44e5fc74cfef70bf2e
Ivan Beschastnikh <bestchai@cs.ubc.ca>
2014-01-20 00:34:02 -0800

    s/dynoptic/csight in a few minor files

--- csight.sh
+++ csight.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Runs Dynoptic from the compiled class files, passing all command
+# Runs CSight from the compiled class files, passing all command
 # line argument directly to main().
 
 java -ea -cp ./lib/junit-4.9b2.jar:./lib/plume.jar:./lib/daikonizer.jar:./lib/synoptic.jar:./dynoptic/bin/ dynoptic.main.DynopticMain $*
