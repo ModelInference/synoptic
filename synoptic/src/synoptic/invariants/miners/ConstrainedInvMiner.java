f87eb7f69cfdff747a4eea45fa9f7df2fd17c0d5
sebastianfiss <sebastianfiss@localhost>
2014-02-17 17:28:54 -0500

    Modified constrained mining (constraint bounds computation) to check all pairs for non-monotonically increasing series

--- synoptic/src/synoptic/invariants/miners/ConstrainedInvMiner.java
+++ synoptic/src/synoptic/invariants/miners/ConstrainedInvMiner.java
@@ -17,7 +17,6 @@ import synoptic.invariants.constraints.UpperBoundConstraint;
 import synoptic.model.ChainsTraceGraph;
 import synoptic.model.EventNode;
 import synoptic.model.Trace;
-import synoptic.model.Transition;
 import synoptic.model.event.Event;
 import synoptic.model.event.EventType;
 import synoptic.model.interfaces.IRelationPath;
@@ -169,9 +168,9 @@ public class ConstrainedInvMiner extends InvariantMiner {
 
         if (inv instanceof InterruptedByInvariant) {
             // IntrBy's constraints are between a&a, not a&b
-            constraints = computeConstraints(a, a, true);
+            constraints = computeUpperLowerConstraints(a, a, true);
         } else {
-            constraints = computeConstraints(a, b, false);
+            constraints = computeUpperLowerConstraints(a, b, false);
         }
 
         // Create two TempConstrainedInvariant objects using the lower bound and
@@ -196,21 +195,16 @@ public class ConstrainedInvMiner extends InvariantMiner {
      * Walks each relationPath and checks for nodes either of EventType a or b.
      * Uses ITime values of these nodes to update and compute a lower and upper
      * bound constraint for an invariant with predicates a and b. The algorithm
-     * to compute an lower bound is as follows: For each relationPath Node
-     * recentA Walking down the trace If see a node of EventType a set recentA
-     * to this node If see a node of EventType b && recentA set obtain delta
-     * (difference between time of this node and recentA) The lower bound is the
-     * min delta value out of all the deltas The algorithm to compute an upper
-     * bound is as follows: For each relationPath Walking down the trace Find
-     * the first node of EventType a Find the last node of EventType b Obtain a
-     * delta value (difference between time of first and last) for relationPath
-     * The upper bound is the max delta value out of all the deltas Returns the
-     * computed lower and upper bound constraints as a pair. The left is the
-     * lower bound constraint and the right is the upper bound constraint. This
-     * method provides an additional param to the overloaded computeConstraints
-     * method: The IntrBy varies as it must compute lower bounds between two
-     * a's. The upper bound is also computed that way, but can reuse the
-     * original implementation. TODO: refactor this at some point
+     * for computing the bounds is as follows: For every type 'a' in each path,
+     * walk all subsequent nodes and retrieve the delta to this node (if 'a' or
+     * 'b' pair). For lower bound, take the minimal value of all deltas, and the
+     * max value for the upper bound. Returns the computed lower and upper bound
+     * constraints as a pair. The left is the lower bound constraint and the
+     * right is the upper bound constraint. This method provides an additional
+     * param to the overloaded computeConstraints method: The IntrBy varies as
+     * it must compute lower bounds between two a's. The upper bound is also
+     * computed that way, but can reuse the original implementation. TODO:
+     * refactor this at some point
      * 
      * @param a
      *            first invariant predicate
@@ -226,7 +220,7 @@ public class ConstrainedInvMiner extends InvariantMiner {
      *         bound constraint and the right represents the upper bound
      *         constraint
      */
-    private Pair<IThresholdConstraint, IThresholdConstraint> computeConstraints(
+    private Pair<IThresholdConstraint, IThresholdConstraint> computeUpperLowerConstraints(
             EventType a, EventType b, boolean betweenFirstAndFirstPredicate) {
 
         ITime lowerBound = null;
@@ -236,81 +230,67 @@ public class ConstrainedInvMiner extends InvariantMiner {
         // Go through each node in path.
         // Find nodes that match event types for invariant.
         // Find upperBound.
-        // Find and lowerBound to create ConstrainedInvariants.
+        // Find lowerBound
+        // create ConstrainedInvariants.
         for (IRelationPath relationPath : relationPaths) {
             EventNode start = relationPath.getFirstNode();
             EventNode end = relationPath.getLastNode();
 
-            // First occurrence of a and last occurrence of b.
-            // last - first = upperBound
-            ITime firstA = null;
-            ITime lastB = null;
-
-            // Track nodes of event type a for computing lowerBound.
-            EventNode recentA = null;
-
-            Transition<EventNode> trans;
-
-            // Current node we're at as we walk the trace.
             EventNode curr = start;
 
+            // Iterate over each node in path
             while (true) {
-                if (curr.getEType().equals(a)) {
-                    // for IntrBy: compute lowerBound between two a's
-                    if (betweenFirstAndFirstPredicate) {
-                        if (recentA != null) {
-                            ITime delta = curr.getTime().computeDelta(
-                                    recentA.getTime());
-                            if (lowerBound == null
-                                    || delta.lessThan(lowerBound)) {
-                                lowerBound = delta;
-                            }
-                        }
-                    }
-                    // mark last recent occurrence of a
-                    recentA = curr;
-                    // mark first occurrence of a (for upper bound)
-                    if (firstA == null) {
-                        firstA = curr.getTime();
-                    }
-                }
-
-                if (curr.getEType().equals(b)) {
-                    // If node of event type a is found already, then we can
-                    // obtain a delta value since we now found node of event
-                    // type b. (NOT FOR IntrBy, lowerBound already covered by
-                    // previous check)
-                    if (recentA != null && !betweenFirstAndFirstPredicate) {
-                        ITime delta = curr.getTime().computeDelta(
-                                recentA.getTime());
-                        if (lowerBound == null || delta.lessThan(lowerBound)) {
-                            lowerBound = delta;
-                        }
-                    }
-                    // mark last occurrence of b (for upper bound)
-                    lastB = curr.getTime();
-                }
-
-                // Dealing with a TO log, so only one transition available to
-                // use.
-                assert (curr.getAllTransitions().size() == 1);
-                trans = curr.getAllTransitions().get(0);
-
                 // Reached ending node in path.
                 if (curr.equals(end)) {
                     break;
                 }
-                curr = trans.getTarget();
-            }
 
-            // relationPath contains the invariant.
-            // Note: this will exclude invariants with an INITIAL node, since
-            // that will yield a null lower-bound and upper-bound.
-            if (firstA != null && lastB != null) {
-                ITime delta = lastB.computeDelta(firstA);
-                if (upperBound == null || upperBound.lessThan(delta)) {
-                    upperBound = delta;
+                // Current node we're at as we walk the trace.
+                assert (curr.getAllTransitions().size() == 1);
+                EventNode next = curr.getAllTransitions().get(0).getTarget();
+
+                // for each node (of type a, else there will be no bound), walk
+                // every subsequent node and check bounds
+                while (curr.getEType().equals(a)) {
+                    // Found (a,a) pair, compute constraints (IntrBy)
+                    if (next.getEType().equals(a)
+                            && betweenFirstAndFirstPredicate) {
+                        ITime delta = next.getTime().computeDelta(
+                                curr.getTime());
+                        if (lowerBound == null || delta.lessThan(lowerBound)) {
+                            lowerBound = delta;
+                        }
+
+                        if (upperBound == null || upperBound.lessThan(delta)) {
+                            upperBound = delta;
+                        }
+                    }
+
+                    // Found (a,b) pair, compute constraints
+                    if (next.getEType().equals(b)
+                            && !betweenFirstAndFirstPredicate) {
+                        ITime delta = next.getTime().computeDelta(
+                                curr.getTime());
+                        if (lowerBound == null || delta.lessThan(lowerBound)) {
+                            lowerBound = delta;
+                        }
+
+                        if (upperBound == null || upperBound.lessThan(delta)) {
+                            upperBound = delta;
+                        }
+                    }
+
+                    // Reached ending node in path.
+                    if (next.equals(end)) {
+                        break;
+                    }
+
+                    assert (next.getAllTransitions().size() == 1);
+                    next = next.getAllTransitions().get(0).getTarget();
                 }
+
+                assert (curr.getAllTransitions().size() == 1);
+                curr = curr.getAllTransitions().get(0).getTarget();
             }
         }
 
