de0c878a866afe4a720bd63a3c0c16601eac2fee
Bob Yang <bobyangyf@gmail.com>
2014-05-23 16:12:01 -0700

    Added stubs to MCRunner class

--- csight/src/csight/mc/MCRunner.java
+++ csight/src/csight/mc/MCRunner.java
@@ -1,11 +1,32 @@
 package csight.mc;
 
+import java.io.IOException;
+import java.util.List;
+
+import csight.invariants.BinaryInvariant;
+
 /**
  * A model-checker process runner class for running multiple
  * mc processes in parallel
  */
 public class MCRunner {
-    // TODO: use Java Executor servie to run multiple mc process
+    // List of Invariants to check
+    private List<BinaryInvariant> invs;
     
-    // TODO: stop all mc process when one terminates
+    public MCRunner(List<BinaryInvariant> invs) {
+        this.invs = invs;
+    }
+    
+    // TODO: use Java Executor service to run multiple mc process
+    public void verify() throws IOException, InterruptedException {
+        //
+    }
+    
+    /**
+     * Stops all MC process and use the first result that is returned
+     * @param mc the finished mc process
+     */
+    public synchronized void onResult(MC mc) {
+        // TODO: stop all mc process when one terminates
+    }
 }
