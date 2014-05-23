3bf261d25b82caf3ef2c0eba9b5079907f9041d5
Bob Yang <bobyangyf@gmail.com>
2014-05-23 15:30:32 -0700

    Added new class required for issue

---
+++ csight/src/csight/mc/MCRunner.java
@@ -0,0 +1,11 @@
+package csight.mc;
+
+/**
+ * A model-checker process runner class for running multiple
+ * mc processes in parallel
+ */
+public class MCRunner {
+    // TODO: use Java Executor servie to run multiple mc process
+    
+    // TODO: stop all mc process when one terminates
+}
