7dc7cc039e643e331a0c14df50b7eb36482e0a7c
Bob Yang <bobyangyf@gmail.com>
2014-05-23 17:06:24 -0700

    Added stubs for MCRunner

--- csight/src/csight/mc/MCRunner.java
+++ csight/src/csight/mc/MCRunner.java
@@ -4,22 +4,39 @@ import java.io.IOException;
 import java.util.List;
 
 import csight.invariants.BinaryInvariant;
+import csight.model.fifosys.gfsm.GFSM;
 
 /**
- * A model-checker process runner class for running multiple
- * mc processes in parallel
+ * A model-checker process runner class for running  and managing
+ * multiple mc processes in parallel
  */
-public class MCRunner {
-    // List of Invariants to check
-    private List<BinaryInvariant> invs;
+public abstract class MCRunner {
+    /** Complete path to the model checker binary (e.g., McScM verify). */
+    protected String mcPath;
     
-    public MCRunner(List<BinaryInvariant> invs) {
-        this.invs = invs;
+    public MCRunner(String mcPath) {
+        this.mcPath = mcPath;
     }
     
-    // TODO: use Java Executor service to run multiple mc process
-    public void verify() throws IOException, InterruptedException {
-        //
+    /**
+     * Runs multiple model checkers in parallel to check the supplied model
+     * against the invariants. Times out the process after timeoutSecs
+     * 
+     * @param pGraph
+     *            - The GFSM model that will be checked and refined to satisfy all
+     *              of the invariants in invsTocheck
+     * @param invs
+     *            - CSight invariants to check and satisfy in pGraph
+     * @param timeOut
+     *            - Seconds to run model checking before timeout
+     * @param minimize
+     *            - whether to minimize each process of the FSM
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public void verify(GFSM pGraph, List<BinaryInvariant> invs, int timeOut,
+            boolean minimize) throws IOException, InterruptedException {
+        // TODO: use Java ExecutorService to run multiple mc process
     }
     
     /**
@@ -29,4 +46,6 @@ public class MCRunner {
     public synchronized void onResult(MC mc) {
         // TODO: stop all mc process when one terminates
     }
+    
+    protected abstract prepareMcInputString()
 }
