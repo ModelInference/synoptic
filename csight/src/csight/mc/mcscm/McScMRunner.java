c2ed8f614c98361f4d883d6d820bb59256be997e
Bob Yang <bobyangyf@gmail.com>
2014-05-27 11:40:58 -0700

    Added McScMRunner class

---
+++ csight/src/csight/mc/mcscm/McScMRunner.java
@@ -0,0 +1,17 @@
+package csight.mc.mcscm;
+
+import csight.mc.MCRunner;
+
+public class McScMRunner extends MCRunner {
+
+    public McScMRunner(String mcPath, int numParallel) {
+        super(mcPath, numParallel);
+    }
+
+    @Override
+    protected void prepareMcInputString() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
