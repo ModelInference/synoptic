9fbb99c2f799054581060851c6e544d4937e6325
Bob Yang <bobyangyf@gmail.com>
2014-06-02 16:59:05 -0700

    Moved method from MCRunner to McScMRunner

--- csight/src/csight/mc/MCRunner.java
+++ csight/src/csight/mc/MCRunner.java
@@ -10,10 +10,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import csight.invariants.BinaryInvariant;
-import csight.mc.mcscm.McScM;
 import csight.model.fifosys.cfsm.CFSM;
 import csight.model.fifosys.gfsm.GFSM;
-import csight.util.Util;
 
 /**
  * A model-checker process runner class for running  and managing
@@ -109,47 +107,19 @@ public abstract class MCRunner {
     /**
      * Returns a list of Callables to run in parallel with ExecutorService
      * given a list of invariants to check
-     * @param pGraph 
+     * @param pGraph
      * @param invs
-     * @param minimize 
+     * @param minimize
      * @return
      */
-    private List<Callable<MCRunnerResult>> getCallablesToRun(final GFSM pGraph,
-            List<BinaryInvariant> invs, final boolean minimize) {
-        List<Callable<MCRunnerResult>> callablesToRun = Util.newList();
-        
-        for (int i=0; i < invs.size() && i < numParallel; i++) {
-            final BinaryInvariant inv = invs.get(i);
-            invsRan.add(inv);
-            
-            Callable<MCRunnerResult> callable = new Callable<MCRunnerResult>() {
-
-                @Override
-                public MCRunnerResult call() throws Exception {
-                    CFSM cfsm = pGraph.getCFSM(minimize);
-                    cfsm.augmentWithInvTracing(inv);
-                    
-                    String mcInputStr = cfsm.toScmString("checking_scm_"
-                            + inv.getConnectorString());
-                    
-                    McScM mcscm = new McScM(mcPath);
-                    mcscm.verifyParallel(mcInputStr);
-                    
-                    MCResult mcResult = mcscm.getVerifyResult(cfsm.getChannelIds());
-                    return new MCRunnerResult(inv, mcResult);
-                }
-                
-            };
-            callablesToRun.add(callable);
-        }
-        return callablesToRun;
-    }
+    protected abstract List<Callable<MCRunnerResult>> getCallablesToRun(final GFSM pGraph,
+            List<BinaryInvariant> invs, final boolean minimize);
     
     /**
      * A result class that stores the completed invariant
      * and its corresponding MCResult
      */
-    private final class MCRunnerResult {
+    protected final class MCRunnerResult {
         
         /** The invariant that was model-checked */
         BinaryInvariant inv;
--- csight/src/csight/mc/mcscm/McScMRunner.java
+++ csight/src/csight/mc/mcscm/McScMRunner.java
@@ -1,9 +1,15 @@
 package csight.mc.mcscm;
 
+import java.util.List;
+import java.util.concurrent.Callable;
+
 import csight.invariants.BinaryInvariant;
 import csight.mc.MC;
+import csight.mc.MCResult;
 import csight.mc.MCRunner;
 import csight.model.fifosys.cfsm.CFSM;
+import csight.model.fifosys.gfsm.GFSM;
+import csight.util.Util;
 
 public class McScMRunner extends MCRunner {
 
@@ -27,4 +33,42 @@ public class McScMRunner extends MCRunner {
                 + curInv.getConnectorString());
     }
 
+    /**
+     * Returns a list of Callables to run in parallel with ExecutorService
+     * given a list of invariants to check
+     * @param pGraph 
+     * @param invs
+     * @param minimize 
+     * @return
+     */
+    protected List<Callable<MCRunnerResult>> getCallablesToRun(final GFSM pGraph,
+            List<BinaryInvariant> invs, final boolean minimize) {
+        List<Callable<MCRunnerResult>> callablesToRun = Util.newList();
+        
+        for (int i=0; i < invs.size() && i < numParallel; i++) {
+            final BinaryInvariant inv = invs.get(i);
+            invsRan.add(inv);
+            
+            Callable<MCRunnerResult> callable = new Callable<MCRunnerResult>() {
+
+                @Override
+                public MCRunnerResult call() throws Exception {
+                    CFSM cfsm = pGraph.getCFSM(minimize);
+                    cfsm.augmentWithInvTracing(inv);
+                    
+                    String mcInputStr = cfsm.toScmString("checking_scm_"
+                            + inv.getConnectorString());
+                    
+                    McScM mcscm = new McScM(mcPath);
+                    mcscm.verifyParallel(mcInputStr);
+                    
+                    MCResult mcResult = mcscm.getVerifyResult(cfsm.getChannelIds());
+                    return new MCRunnerResult(inv, mcResult);
+                }
+                
+            };
+            callablesToRun.add(callable);
+        }
+        return callablesToRun;
+    }
 }
