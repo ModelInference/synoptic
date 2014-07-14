fc21a31fff3825e6b4ceb15115bb41329b1036cd
Ivan Beschastnikh <bestchai@cs.ubc.ca>
2014-07-14 15:36:40 -0700

    minor

--- invarimint.sh
+++ invarimint.sh
@@ -3,4 +3,4 @@
 # Runs InvariMint from the compiled class files, passing all command
 # line argument directly to main().
 
-java -ea -cp ./lib/daikon.jar:./lib/junit-4.9b2.jar:./lib/automaton.jar:./lib/plume.jar:./lib/synoptic.jar:./InvariMint/bin/ main.InvariMintMain $*
+java -ea -cp ./lib/junit-4.9b2.jar:./lib/automaton.jar:./lib/plume.jar:./lib/synoptic.jar:./InvariMint/bin/ main.InvariMintMain $*
