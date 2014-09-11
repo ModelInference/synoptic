03ff71f327f32dd040049ceb15a6385b80a33b87
Bob Yang <bobyangyf@gmail.com>
2014-09-10 22:05:31 -0700

    Added class comments to new classes

--- synoptic/src/synoptic/main/options/ExtPerfumeOptions.java
+++ synoptic/src/synoptic/main/options/ExtPerfumeOptions.java
@@ -1,5 +1,19 @@
 package synoptic.main.options;
 
+/**
+ * <p>
+ * Defines the command line arguments for the extended version of Perfume. It
+ * uses plume-lib for defining command line options, their types, and the
+ * corresponding help messages. This library also provides support for parsing
+ * and populating instances of these options. All options can be exported to
+ * AbstractOptions (the common options for all Synoptic projects) using
+ * toAbstractOptions().
+ * </p>
+ * <p>
+ * Options common between this class and SynopticOptions cannot be pushed up
+ * into a superclass because plume-lib doesn't support inheritance.
+ * </p>
+ */
 public class ExtPerfumeOptions {
     // TODO: fill with options for Extended Perfume
 }
--- synoptic/src/synoptic/main/parser/ExtParser.java
+++ synoptic/src/synoptic/main/parser/ExtParser.java
@@ -1,5 +1,9 @@
 package synoptic.main.parser;
 
+/**
+ * Extended Parser for Perfume to parse multiple performance metrics,
+ * intermediate models, and refinement instructions.
+ */
 public class ExtParser {
     // TODO: parser for extended Perfume functions. Parsing intermediate models,
     // parsing refinement commands.
