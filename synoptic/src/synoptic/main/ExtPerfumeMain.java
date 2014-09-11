6d8af450a08c86d2a7495ac3fd493048447fb4a2
Bob Yang <bobyangyf@gmail.com>
2014-09-10 22:05:31 -0700

    Added class comments to new classes

--- synoptic/src/synoptic/main/ExtPerfumeMain.java
+++ synoptic/src/synoptic/main/ExtPerfumeMain.java
@@ -1,5 +1,10 @@
 package synoptic.main;
 
+/**
+ * The extended PerfumeMain with features to export intermediate models to
+ * files, and perform refinement as specified via an input file, and support
+ * multiple performance metrics per event transition.
+ */
 public class ExtPerfumeMain {
     // TODO: implement with new parsers to run with exporting intermediate
     // models. Modify Perfume so each event can be associated with multiple
@@ -9,4 +14,5 @@ public class ExtPerfumeMain {
 
     // TODO: design the refinement process to allow intermediate export and
     // refinement command.
+
 }
