c5a6278504d0d102bb684c9987f7e4e09abf41ca
Bob Yang <bobyangyf@gmail.com>
2014-09-10 21:26:38 -0700

    Adding TODO and new classes for perfume extension

--- synoptic/src/synoptic/main/ExtPerfumeMain.java
+++ synoptic/src/synoptic/main/ExtPerfumeMain.java
@@ -2,5 +2,11 @@ package synoptic.main;
 
 public class ExtPerfumeMain {
     // TODO: implement with new parsers to run with exporting intermediate
-    // models.
+    // models. Modify Perfume so each event can be associated with multiple
+    // metrics. During refinement process, allow options to output the
+    // intermediate model to file and accept an input file describing the
+    // refinement steps.
+
+    // TODO: design the refinement process to allow intermediate export and
+    // refinement command.
 }
---
+++ synoptic/src/synoptic/main/parser/ExtParser.java
@@ -0,0 +1,8 @@
+package synoptic.main.parser;
+
+public class ExtParser {
+    // TODO: parser for extended Perfume functions. Parsing intermediate models,
+    // parsing refinement commands.
+
+    // TODO: design a communication protocol.
+}
