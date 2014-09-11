2ea4935361200f4d85f59a898f44b86074c35eda
Bob Yang <bobyangyf@gmail.com>
2014-09-10 22:18:23 -0700

    Changed Main to extend Perfume, and added Perfume options to ExtPerfumeOptions

--- synoptic/src/synoptic/main/ExtPerfumeMain.java
+++ synoptic/src/synoptic/main/ExtPerfumeMain.java
@@ -1,11 +1,20 @@
 package synoptic.main;
 
+import synoptic.main.options.AbstractOptions;
+import synoptic.model.export.GraphExportFormatter;
+
 /**
  * The extended PerfumeMain with features to export intermediate models to
  * files, and perform refinement as specified via an input file, and support
  * multiple performance metrics per event transition.
  */
-public class ExtPerfumeMain {
+public class ExtPerfumeMain extends PerfumeMain {
+
+    public ExtPerfumeMain(AbstractOptions opts,
+            GraphExportFormatter graphExportFormatter) {
+        super(opts, graphExportFormatter);
+    }
+
     // TODO: implement with new parsers to run with exporting intermediate
     // models. Modify Perfume so each event can be associated with multiple
     // metrics. During refinement process, allow options to output the
