49aa5b7737f24320becd1504db67d20b7deb2778
Bob Yang <bobyangyf@gmail.com>
2014-09-14 14:25:29 -0700

    Merged Extended Perfume Options with Abstract Options

--- synoptic/src/synoptic/main/ExtPerfumeMain.java
+++ synoptic/src/synoptic/main/ExtPerfumeMain.java
@@ -1,6 +1,11 @@
 package synoptic.main;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import synoptic.main.options.AbstractOptions;
+import synoptic.main.options.ExtPerfumeOptions;
+import synoptic.main.parser.ParseException;
 import synoptic.model.export.GraphExportFormatter;
 
 /**
@@ -13,6 +18,39 @@ public class ExtPerfumeMain extends PerfumeMain {
     public ExtPerfumeMain(AbstractOptions opts,
             GraphExportFormatter graphExportFormatter) {
         super(opts, graphExportFormatter);
+    }
+
+    /**
+     * Parses the set of arguments to the program, to set up static state in
+     * ExtPerfumeMain. This state includes everything necessary to run Extended
+     * Perfume -- input log files, regular expressions, etc. Returns null if
+     * there is a problem with the parsed options.
+     * 
+     * @param args
+     *            Command line arguments that specify how Extended Perfume
+     *            should behave.
+     * @return
+     * @throws IOException
+     * @throws URISyntaxException
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     * @throws ParseException
+     */
+    public static ExtPerfumeMain processArgs(String[] args) throws IOException,
+            URISyntaxException, IllegalArgumentException,
+            IllegalAccessException, ParseException {
+        // Parse and process command line options
+        AbstractOptions options = new ExtPerfumeOptions(args)
+                .toAbstractOptions();
+        GraphExportFormatter graphExportFormatter = processArgs(options);
+        if (graphExportFormatter == null) {
+            return null;
+        }
+
+        // Construct and return main object
+        ExtPerfumeMain newMain = new ExtPerfumeMain(options,
+                graphExportFormatter);
+        return newMain;
     }
 
     // TODO: implement with new parsers to run with exporting intermediate
