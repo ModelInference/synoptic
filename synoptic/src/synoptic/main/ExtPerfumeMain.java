3ee1424006d00cf95fcd2a83f603afd4592e010c
Bob Yang <bobyangyf@gmail.com>
2014-09-14 20:45:19 -0700

    Modified ExtPerfumeMain and added method stubs

--- synoptic/src/synoptic/main/ExtPerfumeMain.java
+++ synoptic/src/synoptic/main/ExtPerfumeMain.java
@@ -3,9 +3,12 @@ package synoptic.main;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import synoptic.invariants.TemporalInvariantSet;
 import synoptic.main.options.AbstractOptions;
 import synoptic.main.options.ExtPerfumeOptions;
 import synoptic.main.parser.ParseException;
+import synoptic.model.ChainsTraceGraph;
+import synoptic.model.PartitionGraph;
 import synoptic.model.export.GraphExportFormatter;
 
 /**
@@ -14,6 +17,16 @@ import synoptic.model.export.GraphExportFormatter;
  * multiple performance metrics per event transition.
  */
 public class ExtPerfumeMain extends PerfumeMain {
+
+    /**
+     * Return the singleton instance of ExtPerfumeMain, first asserting that the
+     * instance isn't null.
+     */
+    public static ExtPerfumeMain getInstance() {
+        assert (instance != null);
+        assert (instance instanceof ExtPerfumeMain);
+        return (ExtPerfumeMain) instance;
+    }
 
     public ExtPerfumeMain(AbstractOptions opts,
             GraphExportFormatter graphExportFormatter) {
@@ -53,13 +66,40 @@ public class ExtPerfumeMain extends PerfumeMain {
         return newMain;
     }
 
-    // TODO: implement with new parsers to run with exporting intermediate
-    // models. Modify Perfume so each event can be associated with multiple
-    // metrics. During refinement process, allow options to output the
-    // intermediate model to file and accept an input file describing the
-    // refinement steps.
+    /**
+     * Mines and returns the totally ordered invariants from the trace graph of
+     * the input log. Extended Perfume will mine multiple performance metrics
+     * per event.
+     * 
+     * @param useTransitiveClosureMining
+     * @param traceGraph
+     * @return
+     */
+    @Override
+    public TemporalInvariantSet mineTOInvariants(
+            boolean useTransitiveClosureMining, ChainsTraceGraph traceGraph) {
+        // TODO: implement with new parsers to run with exporting intermediate
+        // models. Modify Perfume so each event can be associated with multiple
+        // metrics. During refinement process, allow options to output the
+        // intermediate model to file and accept an input file describing the
+        // refinement steps.
+        return null;
+    }
 
-    // TODO: design the refinement process to allow intermediate export and
-    // refinement command.
+    /**
+     * Runs the Extended Perfume Synoptic algorithm starting from the initial
+     * graph (pGraph). The pGraph is assumed to be fully initialized and ready
+     * for refinement. The Extended Perfume Synoptic algorithm outputs an
+     * intermediate model and waits for refinement instructions until the model
+     * is fully refined, or until instructed to stop.
+     * 
+     * @param pGraph
+     *            The initial graph model to start refining.
+     */
+    @Override
+    public void runSynoptic(PartitionGraph pGraph) {
+        // TODO: design the refinement process to allow intermediate export and
+        // refinement command.
+    }
 
 }
