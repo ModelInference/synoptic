9e62846fdf56509dec7a57731f2fe4055015c2b2
Bob Yang <bobyangyf@gmail.com>
2015-01-26 13:39:49 -0800

    Added resource type to Synoptic for multiresource perfume

---
+++ synoptic/src/synoptic/model/resource/Resource.java
@@ -0,0 +1,38 @@
+package synoptic.model.resource;
+
+/**
+ * Represents a mined resource with the resource type and resource value for
+ * Perfume. A resource is a key identifying the resource, and a number (any of
+ * int, float, double etc)
+ */
+public class Resource<E extends Number & Comparable<E>> implements
+        Comparable<Resource<E>> {
+
+    /**
+     * The resource type used to identify this resource
+     */
+    private String resourceType;
+
+    /**
+     * The value of this resource
+     */
+    private E resourceVal;
+
+    public Resource(String type, E val) {
+        resourceType = type;
+        resourceVal = val;
+    }
+
+    @Override
+    public int compareTo(Resource<E> o) {
+        return resourceVal.compareTo(o.resourceVal);
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public E getResourceVal() {
+        return resourceVal;
+    }
+}
