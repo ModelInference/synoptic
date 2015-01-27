1836ac65888c9dbda349b644fc1ee00c77cb4361
Bob Yang <bobyangyf@gmail.com>
2015-01-27 11:37:56 -0800

    Added generic type to resource constraint

--- synoptic/src/synoptic/invariants/constraints/IResourceConstraint.java
+++ synoptic/src/synoptic/invariants/constraints/IResourceConstraint.java
@@ -1,19 +1,19 @@
 package synoptic.invariants.constraints;
 
-import synoptic.model.resource.Resource;
+import synoptic.model.resource.IResource;
 
 /**
  * Represents a resource threshold constraint on some resource invariant.
  */
-public interface IResourceConstraint {
+public interface IResourceConstraint<E extends Comparable<E>> {
     /**
      * @return resource constraint
      */
-    Resource<?> getThreshold();
+    IResource<E> getThreshold();
 
     /**
      * @return true if given resource satisfies internal inequality with respect
      *         to the threshold.
      */
-    boolean evaluate(Resource<?> t);
+    boolean evaluate(IResource<E> t);
 }
